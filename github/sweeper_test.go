6dc4d417d7d2cea06d4ac6bfac4b0856230817e2
stack72 <public@paulstack.co.uk>
2017-07-06 13:00:56 +0300

    cleanup: Adding sweepers to github code
    ```
    % go test ./github -v -sweep=gh-region
    2017/07/06 12:58:45 [DEBUG] Running Sweepers for region (gh-region):
    2017/07/06 12:58:46 Destroying Repository tf-acc-test-h0pi74ytcx
    2017/07/06 12:58:46 Destroying Repository tf-acc-test-hnyw79at3m
    2017/07/06 12:58:46 Destroying Repository tf-acc-test-iqg1yjjqk7
    2017/07/06 12:58:46 Destroying Repository tf-acc-test-jqf6llitnq
    2017/07/06 12:58:47 Destroying Repository tf-acc-test-jyk2v80gcw
    2017/07/06 12:58:47 Destroying Repository tf-acc-test-k9q6gx8xhy
    2017/07/06 12:58:47 Destroying Repository tf-acc-test-lvba4stwvd
    2017/07/06 12:58:47 Destroying Repository tf-acc-test-m7vx7zvv0a
    2017/07/06 12:58:48 Destroying Repository tf-acc-test-mhkuudhb11
    2017/07/06 12:58:48 Destroying Repository tf-acc-test-o79z42f3bm
    2017/07/06 12:58:48 Destroying Repository tf-acc-test-pvzrx7q8aw
    2017/07/06 12:58:48 Destroying Repository tf-acc-test-qd04l2p4mm
    2017/07/06 12:58:49 Destroying Repository tf-acc-test-ql9hbu7i7v
    2017/07/06 12:58:49 Destroying Repository tf-acc-test-smn0iru3cp
    2017/07/06 12:58:49 Destroying Repository tf-acc-test-t6vhwy8fax
    2017/07/06 12:58:49 Destroying Repository tf-acc-test-team-7cblt
    2017/07/06 12:58:50 Destroying Repository tf-acc-test-team-a06n7
    2017/07/06 13:00:09 Destroying Repository foo-29aite2a3x
    2017/07/06 13:00:11 Destroying Repository foo-2xlsxz264q
    2017/07/06 13:00:11 Destroying Repository foo-6rwqrcyilt
    2017/07/06 13:00:11 Destroying Repository foo-f68q6tramj
    2017/07/06 13:00:12 Destroying Repository foo-kdy7x0efx3
    2017/07/06 13:00:13 Destroying Repository foo-lni9twzaz1
    2017/07/06 13:00:13 Destroying Repository foo-lwhtylv1bd
    2017/07/06 13:00:14 Destroying Repository foo-mmliryzy6p
    2017/07/06 13:00:14 Destroying Repository foo-qm9p81rgmv
    2017/07/06 13:00:15 Destroying Repository foo-vwgoo0hobj
    2017/07/06 12:58:50 Sweeper Tests ran:
    	- github_repository
    ok  	github.com/terraform-providers/terraform-provider-github/github	4.889s
    ```

---
+++ github/sweeper_test.go
@@ -0,0 +1,36 @@
+package github
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestMain(m *testing.M) {
+	resource.TestMain(m)
+}
+
+func sharedConfigForRegion(region string) (interface{}, error) {
+	if os.Getenv("GITHUB_TOKEN") == "" {
+		return nil, fmt.Errorf("empty GITHUB_TOKEN")
+	}
+
+	if os.Getenv("GITHUB_ORGANIZATION") == "" {
+		return nil, fmt.Errorf("empty GITHUB_ORGANIZATION")
+	}
+
+	config := Config{
+		Token:        os.Getenv("GITHUB_TOKEN"),
+		Organization: os.Getenv("GITHUB_ORGANIZATION"),
+		BaseURL:      "",
+	}
+
+	client, err := config.Client()
+	if err != nil {
+		return nil, fmt.Errorf("error getting Github client")
+	}
+
+	return client, nil
+}
