f0c66deaa73b99229662b7b73fe19cc1ca9b8b83
Eugene Taranov <eugene@taranov.me>
2018-01-18 15:26:05 +0300

    disabled forcenew for repo name

--- github/resource_github_repository.go
+++ github/resource_github_repository.go
@@ -24,7 +24,7 @@ func resourceGithubRepository() *schema.Resource {
 			"name": {
 				Type:     schema.TypeString,
 				Required: true,
-				ForceNew: true,
+				ForceNew: false,
 			},
 			"description": {
 				Type:     schema.TypeString,
