acba77aee7ef2f47fd21f0fb4f3f319f8ee4d3bd
megan07 <megan07@users.noreply.github.com>
2019-06-10 15:38:15 -0500

    New Datasource: `github_collaborators` (#239)

---
+++ github/data_source_github_collaborators_test.go
@@ -0,0 +1,42 @@
+package github
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccGithubCollaboratorsDataSource_basic(t *testing.T) {
+	repoName := fmt.Sprintf("tf-acc-test-collab-%s", acctest.RandString(5))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckGithubCollaboratorsDataSourceConfig(repoName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.github_collaborators.test", "collaborator.#"),
+					resource.TestCheckResourceAttr("data.github_collaborators.test", "affiliation", "all"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckGithubCollaboratorsDataSourceConfig(repo string) string {
+	return fmt.Sprintf(`
+resource "github_repository" "test" {
+    name = "%s"
+}
+
+data "github_collaborators" "test" {
+    owner       = "%s"
+    repository  = "${github_repository.test.name}"
+}
+`, repo, testOrganization)
+}
