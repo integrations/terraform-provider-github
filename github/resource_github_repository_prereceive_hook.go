998c86b6d25a8764c420715b97c0fa95e952b3a2
Joe Stump <joe.stump@salesforce.com>
2019-07-02 14:18:41 -0700

    Introduce a working github_repository_prereceive_hook resource.

---
+++ github/resource_github_repository_prereceive_hook.go
@@ -0,0 +1,160 @@
+package github
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/google/go-github/v25/github"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/helper/validation"
+)
+
+func resourceGithubRepositoryPreReceiveHook() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceGithubRepositoryPreReceiveHookCreate,
+		Read:   resourceGithubRepositoryPreReceiveHookRead,
+		Update: resourceGithubRepositoryPreReceiveHookCreate,
+		Delete: resourceGithubRepositoryPreReceiveHookDelete,
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"repository": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"enforcement": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringInSlice([]string{"enabled", "disabled", "testing"}, false),
+			},
+			"config_url": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+		},
+	}
+}
+
+func fetchGitHubRepositoryPreReceiveHookByName(meta interface{}, repoName, hookName string) (*github.PreReceiveHook, error) {
+	ctx := context.Background()
+	client := meta.(*Organization).client
+	orgName := meta.(*Organization).name
+
+	opt := &github.ListOptions{
+		PerPage: 100,
+	}
+
+	var hook *github.PreReceiveHook
+
+	for {
+		hooks, resp, err := client.Repositories.ListPreReceiveHooks(ctx, orgName, repoName, opt)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, h := range hooks {
+			n := *h.Name
+			if n == hookName {
+				hook = h
+				break
+			}
+		}
+
+		if resp.NextPage == 0 {
+			break
+		}
+		opt.Page = resp.NextPage
+	}
+
+	if *hook.ID <= 0 {
+		return nil, fmt.Errorf("No pre-receive hook with name %s found on %s/%s", hookName, orgName, repoName)
+	}
+
+	return hook, nil
+}
+
+func resourceGithubRepositoryPreReceiveHookCreate(d *schema.ResourceData, meta interface{}) error {
+	err := checkOrganization(meta)
+	if err != nil {
+		return err
+	}
+
+	repoName := d.Get("repository").(string)
+	hookName := d.Get("name").(string)
+
+	hook, err := fetchGitHubRepositoryPreReceiveHookByName(meta, repoName, hookName)
+	if err != nil {
+		return err
+	}
+
+	enforcement := d.Get("enforcement").(string)
+	hook.Enforcement = &enforcement
+
+	configURL := d.Get("config_url").(string)
+	hook.ConfigURL = &configURL
+
+	ctx := context.Background()
+	client := meta.(*Organization).client
+	orgName := meta.(*Organization).name
+	_, _, err = client.Repositories.UpdatePreReceiveHook(ctx, orgName, repoName, *hook.ID, hook)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(fmt.Sprintf("%s-%s-%s", orgName, repoName, strconv.FormatInt(*hook.ID, 10)))
+
+	return resourceGithubRepositoryPreReceiveHookRead(d, meta)
+}
+
+func resourceGithubRepositoryPreReceiveHookRead(d *schema.ResourceData, meta interface{}) error {
+	err := checkOrganization(meta)
+	if err != nil {
+		return err
+	}
+
+	repoName := d.Get("repository").(string)
+	hookName := d.Get("name").(string)
+
+	hook, err := fetchGitHubRepositoryPreReceiveHookByName(meta, repoName, hookName)
+	if err != nil {
+		return err
+	}
+
+	d.Set("enforcement", hook.Enforcement)
+	d.Set("config_url", hook.ConfigURL)
+
+	return nil
+}
+
+func resourceGithubRepositoryPreReceiveHookDelete(d *schema.ResourceData, meta interface{}) error {
+	err := checkOrganization(meta)
+	if err != nil {
+		return err
+	}
+
+	repoName := d.Get("repository").(string)
+	hookName := d.Get("name").(string)
+
+	hook, err := fetchGitHubRepositoryPreReceiveHookByName(meta, repoName, hookName)
+	if err != nil {
+		return err
+	}
+
+	disabled := "disabled"
+	hook.Enforcement = &disabled
+
+	ctx := context.Background()
+	client := meta.(*Organization).client
+	orgName := meta.(*Organization).name
+	_, _, err = client.Repositories.UpdatePreReceiveHook(ctx, orgName, repoName, *hook.ID, hook)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
