9b4e9b31f98789a55063f593f6df4961f5489b7a
Patrick Marabeas <patrick@marabeas.io>
2020-01-31 10:06:45 +1100

    Uplift user data source to use v4 API

--- github/data_source_github_user.go
+++ github/data_source_github_user.go
@@ -2,149 +2,88 @@ package github
 
 import (
 	"context"
-	"log"
-	"strconv"
+	"fmt"
 
 	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/shurcooL/githubv4"
 )
+
+const (
+	USER_EMAIL         = "email"
+	USER_IS_SITE_ADMIN = "is_site_admin"
+	USER_LOGIN         = "login"
+	USER_NAME          = "name"
+)
+
+type User struct {
+	Email       githubv4.String
+	ID          githubv4.ID
+	IsSiteAdmin githubv4.Boolean
+	Login       githubv4.String
+	Name        githubv4.String
+}
 
 func dataSourceGithubUser() *schema.Resource {
 	return &schema.Resource{
-		Read: dataSourceGithubUserRead,
+		SchemaVersion: 1,
 
 		Schema: map[string]*schema.Schema{
-			"username": {
-				Type:     schema.TypeString,
-				Required: true,
+			// Input
+			USER_LOGIN: {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "",
 			},
-			"login": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"avatar_url": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"gravatar_id": {
+			// Computed
+			USER_EMAIL: {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
-			"site_admin": {
+			USER_IS_SITE_ADMIN: {
 				Type:     schema.TypeBool,
 				Computed: true,
 			},
-			"name": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"company": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"blog": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"location": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"email": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"bio": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"gpg_keys": {
-				Type:     schema.TypeList,
-				Computed: true,
-				Elem:     &schema.Schema{Type: schema.TypeString},
-			},
-			"ssh_keys": {
-				Type:     schema.TypeList,
-				Computed: true,
-				Elem:     &schema.Schema{Type: schema.TypeString},
-			},
-			"public_repos": {
-				Type:     schema.TypeInt,
-				Computed: true,
-			},
-			"public_gists": {
-				Type:     schema.TypeInt,
-				Computed: true,
-			},
-			"followers": {
-				Type:     schema.TypeInt,
-				Computed: true,
-			},
-			"following": {
-				Type:     schema.TypeInt,
-				Computed: true,
-			},
-			"created_at": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"updated_at": {
+			USER_NAME: {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
 		},
+
+		Read: dataSourceGithubUserRead,
 	}
 }
 
 func dataSourceGithubUserRead(d *schema.ResourceData, meta interface{}) error {
-	username := d.Get("username").(string)
-	log.Printf("[INFO] Refreshing GitHub User: %s", username)
+	var query struct {
+		User User `graphql:"user(login: $login)"`
+	}
+	variables := map[string]interface{}{
+		"login": githubv4.String(d.Get(USER_LOGIN).(string)),
+	}
 
-	client := meta.(*Organization).v3client
 	ctx := context.Background()
-
-	user, _, err := client.Users.Get(ctx, username)
+	client := meta.(*Organization).v4client
+	err := client.Query(ctx, &query, variables)
 	if err != nil {
 		return err
 	}
 
-	gpg, _, err := client.Users.ListGPGKeys(ctx, username, nil)
-	if err != nil {
-		return err
-	}
-	ssh, _, err := client.Users.ListKeys(ctx, username, nil)
+	err = d.Set(USER_EMAIL, query.User.Email)
 	if err != nil {
 		return err
 	}
 
-	gpgKeys := []string{}
-	for _, v := range gpg {
-		gpgKeys = append(gpgKeys, v.GetPublicKey())
+	err = d.Set(USER_IS_SITE_ADMIN, query.User.IsSiteAdmin)
+	if err != nil {
+		return err
 	}
 
-	sshKeys := []string{}
-	for _, v := range ssh {
-		sshKeys = append(sshKeys, v.GetKey())
+	err = d.Set(USER_NAME, query.User.Name)
+	if err != nil {
+		return err
 	}
 
-	d.SetId(strconv.FormatInt(user.GetID(), 10))
-	d.Set("login", user.GetLogin())
-	d.Set("avatar_url", user.GetAvatarURL())
-	d.Set("gravatar_id", user.GetGravatarID())
-	d.Set("site_admin", user.GetSiteAdmin())
-	d.Set("company", user.GetCompany())
-	d.Set("blog", user.GetBlog())
-	d.Set("location", user.GetLocation())
-	d.Set("name", user.GetName())
-	d.Set("email", user.GetEmail())
-	d.Set("bio", user.GetBio())
-	d.Set("gpg_keys", gpgKeys)
-	d.Set("ssh_keys", sshKeys)
-	d.Set("public_repos", user.GetPublicRepos())
-	d.Set("public_gists", user.GetPublicGists())
-	d.Set("followers", user.GetFollowers())
-	d.Set("following", user.GetFollowing())
-	d.Set("created_at", user.GetCreatedAt())
-	d.Set("updated_at", user.GetUpdatedAt())
+	d.SetId(fmt.Sprintf("%s", query.User.ID))
 
 	return nil
 }
