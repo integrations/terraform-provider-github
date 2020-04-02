78d73f9dc0bac2751805ef7ea7327202b6dc57b0
Angie Pinilla <angelinepinilla@gmail.com>
2020-04-02 15:53:17 -0400

    remove nomatch test for data source

--- github/data_source_github_organization_team_sync_groups_test.go
+++ github/data_source_github_organization_team_sync_groups_test.go
@@ -1,30 +1,10 @@
 package github
 
 import (
-	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
-
-func TestAccGithubOrganizationTeamSyncGroupsDataSource_noMatchReturnsError(t *testing.T) {
-	if isEnterprise != "true" {
-		t.Skip("Skipping because `ENTERPRISE_ACCOUNT` is not set or set to false")
-	}
-
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck: func() {
-			testAccPreCheck(t)
-		},
-		Providers: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config:      testAccCheckGithubOrganizationTeamSyncGroupsDataSourceConfig(),
-				ExpectError: regexp.MustCompile(`This team is not externally managed.`),
-			},
-		},
-	})
-}
 
 func TestAccGithubOrganizationTeamSyncGroupsDataSource_existing(t *testing.T) {
 	if isEnterprise != "true" {
