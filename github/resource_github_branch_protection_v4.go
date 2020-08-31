e1752ba1631fbdc507431dd8863e8cf34218bcc9
Patrick Marabeas <patrick@marabeas.io>
2020-08-31 14:09:58 +1000

    fixup! Uplift branch_protection resource to use v4 API

--- github/resource_github_branch_protection_v4.go
+++ github/resource_github_branch_protection_v4.go
@@ -3,8 +3,8 @@ package github
 import (
 	"context"
 	"fmt"
-	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/hashicorp/terraform/helper/validation"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/shurcooL/githubv4"
 	"log"
 	"strings"
