f77afa2279d3bf730a44230babda52d6816d7ce4
Jeremy Udit <jcudit@github.com>
2020-09-01 20:02:05 -0400

    Add Provider-Level Testing

--- github/provider_test.go
+++ github/provider_test.go
@@ -2,10 +2,6 @@ package github
 
 import (
 	"fmt"
-	"math/rand"
-	"os"
-	"path/filepath"
-	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
@@ -56,98 +52,65 @@ func TestProvider(t *testing.T) {
 
 func TestAccProviderConfigure(t *testing.T) {
 
-	t.Run("can be configured to run insecurely", func(t *testing.T) {
+	t.Run("can be configured to run anonymously", func(t *testing.T) {
 
-		// Use ephemeral port range (49152–65535)
-		port := fmt.Sprintf("%d", 49152+rand.Intn(16382))
+		config := `
+			provider "github" {}
+		`
 
-		// Use self-signed certificate
-		certFile := filepath.Join("test-fixtures", "cert.pem")
-		keyFile := filepath.Join("test-fixtures", "key.pem")
-
-		url, closeFunc := githubTLSApiMock(port, certFile, keyFile, t)
-		defer func() {
-			err := closeFunc()
-			if err != nil {
-				t.Fatal(err)
-			}
-		}()
-
-		oldBaseUrl := os.Getenv("GITHUB_BASE_URL")
-		defer os.Setenv("GITHUB_BASE_URL", oldBaseUrl)
-
-		// Point provider to mock API with self-signed cert
-		os.Setenv("GITHUB_BASE_URL", url)
-
-		providerConfig := `provider "github" {}`
-
-		username := "hashibot"
 		resource.Test(t, resource.TestCase{
+			PreCheck:  func() { skipUnlessMode(t, anonymous) },
 			Providers: testAccProviders,
 			Steps: []resource.TestStep{
 				{
-					Config:      providerConfig + testAccCheckGithubUserDataSourceConfig(username),
-					ExpectError: regexp.MustCompile("x509: certificate is valid for untrusted, not localhost"),
+					Config:             config,
+					ExpectNonEmptyPlan: false,
 				},
 			},
 		})
 
 	})
 
-	t.Run("can be configured to run anonymously", func(t *testing.T) {
+	t.Run("can be configured to run insecurely", func(t *testing.T) {
 
-		anonymousConfiguration := `
-			provider "github" {}
-			data "github_ip_ranges" "test" {}
-		`
-		anonymousCheck := resource.ComposeTestCheckFunc(
-			resource.TestCheckResourceAttrSet("data.github_ip_ranges.test", "hooks.#"),
-			resource.TestCheckResourceAttrSet("data.github_ip_ranges.test", "git.#"),
-			resource.TestCheckResourceAttrSet("data.github_ip_ranges.test", "pages.#"),
-			resource.TestCheckResourceAttrSet("data.github_ip_ranges.test", "importer.#"),
+		config := fmt.Sprintf(`
+				provider "github" {
+					token = "%s"
+					insecure = true
+				}`,
+			testToken,
 		)
 
 		resource.Test(t, resource.TestCase{
-			PreCheck:  func() {},
+			PreCheck:  func() { skipUnlessMode(t, anonymous) },
 			Providers: testAccProviders,
 			Steps: []resource.TestStep{
 				{
-					Config: anonymousConfiguration,
-					Check:  anonymousCheck,
+					Config:             config,
+					ExpectNonEmptyPlan: false,
 				},
 			},
 		})
+
 	})
 
 	t.Run("can be configured with an individual account", func(t *testing.T) {
 
-		individualConfiguration := fmt.Sprintf(`
+		config := fmt.Sprintf(`
 			provider "github" {
 				token = "%s"
-			}
-			data "github_user" "test" { username = "%s" }
-		`,
-			os.Getenv("GITHUB_TOKEN"),
-			os.Getenv("GITHUB_ORGANIZATION"),
-		)
-
-		individualCheck := resource.ComposeTestCheckFunc(
-			resource.TestCheckResourceAttrSet("data.github_user.test", "username"),
+				owner = "%s"
+			}`,
+			testToken, testOwnerFunc(),
 		)
 
 		resource.Test(t, resource.TestCase{
-			PreCheck: func() {
-				requiredEnvironmentVariables := []string{
-					"GITHUB_TOKEN",
-					"GITHUB_OWNER",
-				}
-				testAccPreCheckEnvironment(t, requiredEnvironmentVariables)
-			},
+			PreCheck:  func() { skipUnlessMode(t, individual) },
 			Providers: testAccProviders,
 			Steps: []resource.TestStep{
 				{
-					Config: individualConfiguration,
-					Check:  individualCheck,
+					Config:             config,
+					ExpectNonEmptyPlan: false,
 				},
 			},
 		})
@@ -156,72 +119,44 @@ func TestAccProviderConfigure(t *testing.T) {
 
 	t.Run("can be configured with an organization account", func(t *testing.T) {
 
-		organizationConfiguration := fmt.Sprintf(`
+		config := fmt.Sprintf(`
 			provider "github" {
-				organization = "%s"
 				token = "%s"
-			}
-			data "github_organization" "test" { name = "%s" }
-		`,
-			os.Getenv("GITHUB_ORGANIZATION"),
-			os.Getenv("GITHUB_TOKEN"),
-			os.Getenv("GITHUB_ORGANIZATION"),
-		)
-
-		organizationCheck := resource.ComposeTestCheckFunc(
-			resource.TestCheckResourceAttrSet("data.github_organization.test", "plan"),
+				organization = "%s"
+			}`,
+			testToken, testOrganizationFunc(),
 		)
 
 		resource.Test(t, resource.TestCase{
-			PreCheck: func() {
-				requiredEnvironmentVariables := []string{
-					"GITHUB_ORGANIZATION",
-					"GITHUB_TOKEN",
-				}
-				testAccPreCheckEnvironment(t, requiredEnvironmentVariables)
-			},
+			PreCheck:  func() { skipUnlessMode(t, organization) },
 			Providers: testAccProviders,
 			Steps: []resource.TestStep{
 				{
-					Config: organizationConfiguration,
-					Check:  organizationCheck,
+					Config:             config,
+					ExpectNonEmptyPlan: false,
 				},
 			},
 		})
+
 	})
 
 	t.Run("can be configured with a GHES deployment", func(t *testing.T) {
 
-		organizationConfiguration := fmt.Sprintf(`
-				provider "github" {
-					base_url = "%s"
-					token = "%s"
-				}
-				data "github_organization" "test" { name = "%s" }
-			`,
-			os.Getenv("GHES_BASE_URL"),
-			os.Getenv("GHES_TOKEN"),
-			os.Getenv("GHES_ORGANIZATION"),
-		)
-
-		organizationCheck := resource.ComposeTestCheckFunc(
-			resource.TestCheckResourceAttrSet("data.github_organization.test", "plan"),
+		config := fmt.Sprintf(`
+			provider "github" {
+				token = "%s"
+				base_url = "%s"
+			}`,
+			testToken, testBaseURLGHES,
 		)
 
 		resource.Test(t, resource.TestCase{
-			PreCheck: func() {
-				requiredEnvironmentVariables := []string{
-					"GHES_TOKEN",
-					"GHES_BASE_URL",
-					"GHES_ORGANIZATION",
-				}
-				testAccPreCheckEnvironment(t, requiredEnvironmentVariables)
-			},
+			PreCheck:  func() { skipUnlessMode(t, individual) },
 			Providers: testAccProviders,
 			Steps: []resource.TestStep{
 				{
-					Config: organizationConfiguration,
-					Check:  organizationCheck,
+					Config:             config,
+					ExpectNonEmptyPlan: false,
 				},
 			},
 		})
