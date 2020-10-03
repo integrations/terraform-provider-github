cdcdd893b2e9c2569403236fb72322eb2bf77379
Jeremy Udit <jcudit@github.com>
2020-10-03 08:55:06 -0400

    declare branch explicitly to work around error
    >  Branch master not found in repository or repository is not readable

--- github/resource_github_repository_file_test.go
+++ github/resource_github_repository_file_test.go
@@ -17,28 +17,33 @@ func TestAccGithubRepositoryFile(t *testing.T) {
 
 		config := fmt.Sprintf(`
 
-			resource "github_repository" "foo" {
-			  name      = "tf-acc-test-%s"
-			  auto_init = true
+
+			resource "github_repository" "test" {
+				name      = "tf-acc-test-%s"
+				auto_init = true
 			}
 
-			resource "github_repository_file" "foo" {
-			  repository     = github_repository.foo.name
-			  branch         = "master"
-			  file           = "foo"
-			  content        = "bar"
-			  commit_message = "Managed by Terraform"
-			  commit_author  = "Terraform User"
-			  commit_email   = "terraform@example.com"
-			  overwrite      = true
+			resource "github_branch" "test" {
+				repository = github_repository.test.id
+				branch     = "tf-acc-test-%[1]s"
+			}
+
+			resource "github_repository_file" "test" {
+				repository     = github_repository.test.name
+				branch         = github_branch.test.branch
+				file           = "test"
+				content        = "bar"
+				commit_message = "Managed by Terraform"
+				commit_author  = "Terraform User"
+				commit_email   = "terraform@example.com"
+				overwrite      = true
 			}
 
 			resource "github_repository_file" "gitignore" {
-			  repository = "example"
-			  file       = ".gitignore"
-			  content    = "**/*.tfstate"
+				repository = github_repository.test.name
+				file       = ".gitignore"
+				content    = "**/*.tfstate"
 			}
-
 		`, randomID)
 
 		check := resource.ComposeTestCheckFunc(
@@ -74,4 +79,66 @@ func TestAccGithubRepositoryFile(t *testing.T) {
 		})
 
 	})
+
+	// t.Run("can be configured to overwrite files on create", func(t *testing.T) {
+	//
+	// 	config := fmt.Sprintf(`
+	//
+	// 		resource "github_repository" "foo" {
+	// 		  name      = "tf-acc-test-%s"
+	// 		  auto_init = true
+	// 		}
+	//
+	// 		resource "github_repository_file" "foo" {
+	// 		  repository     = github_repository.foo.name
+	// 		  branch         = "master"
+	// 		  file           = "foo"
+	// 		  content        = "bar"
+	// 		  commit_message = "Managed by Terraform"
+	// 		  commit_author  = "Terraform User"
+	// 		  commit_email   = "terraform@example.com"
+	// 		  overwrite      = true
+	// 		}
+	//
+	// 		resource "github_repository_file" "gitignore" {
+	// 		  repository = "example"
+	// 		  file       = ".gitignore"
+	// 		  content    = "**/*.tfstate"
+	// 		}
+	//
+	// 	`, randomID)
+	//
+	// 	check := resource.ComposeTestCheckFunc(
+	// 		resource.TestMatchResourceAttr(
+	// 			"github_repository_file.test", "url",
+	// 			regexp.MustCompile(randomID+"/projects/1"),
+	// 		),
+	// 	)
+	//
+	// 	testCase := func(t *testing.T, mode string) {
+	// 		resource.Test(t, resource.TestCase{
+	// 			PreCheck:  func() { skipUnlessMode(t, mode) },
+	// 			Providers: testAccProviders,
+	// 			Steps: []resource.TestStep{
+	// 				{
+	// 					Config: config,
+	// 					Check:  check,
+	// 				},
+	// 			},
+	// 		})
+	// 	}
+	//
+	// 	t.Run("with an anonymous account", func(t *testing.T) {
+	// 		t.Skip("anonymous account not supported for this operation")
+	// 	})
+	//
+	// 	t.Run("with an individual account", func(t *testing.T) {
+	// 		testCase(t, individual)
+	// 	})
+	//
+	// 	t.Run("with an organization account", func(t *testing.T) {
+	// 		testCase(t, organization)
+	// 	})
+	//
+	// })
 }
