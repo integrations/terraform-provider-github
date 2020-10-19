795a6fd7f0cebac93a110930d6ed2c6e5e35c3a2
Jeremy Udit <jcudit@github.com>
2020-10-19 09:51:01 -0400

    measure timing

--- github/resource_github_repository_file_test.go
+++ github/resource_github_repository_file_test.go
@@ -2,8 +2,6 @@ package github
 
 import (
 	"fmt"
-	"regexp"
-	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
@@ -72,65 +70,65 @@ func TestAccGithubRepositoryFile(t *testing.T) {
 
 	})
 
-	t.Run("can be configured to overwrite files on create", func(t *testing.T) {
-
-		config := fmt.Sprintf(`
-			resource "github_repository" "test" {
-			  name      = "tf-acc-test-%s"
-			  auto_init = true
-			}
-
-			resource "github_repository_file" "test" {
-				repository          = github_repository.test.name
-				branch              = "main"
-				file                = "README.md"
-				content             = "overwritten"
-				overwrite_on_create = false
-			}
-
-		`, randomID)
-
-		check := resource.ComposeTestCheckFunc(
-			resource.TestCheckResourceAttr(
-				"github_repository_file.test", "content",
-				"overwritten",
-			),
-			resource.TestCheckResourceAttr(
-				"github_repository_file.test", "sha",
-				"67c1a95c2d9bb138aefeaebb319cca82e531736b",
-			),
-		)
-
-		testCase := func(t *testing.T, mode string) {
-			resource.Test(t, resource.TestCase{
-				PreCheck:  func() { skipUnlessMode(t, mode) },
-				Providers: testAccProviders,
-				Steps: []resource.TestStep{
-					{
-						Config:      config,
-						ExpectError: regexp.MustCompile(`Refusing to overwrite existing file`),
-					},
-					{
-						Config: strings.Replace(config,
-							"overwrite_on_create = false",
-							"overwrite_on_create = true", 1),
-						Check: check,
-					},
-				},
-			})
-		}
-
-		t.Run("with an anonymous account", func(t *testing.T) {
-			t.Skip("anonymous account not supported for this operation")
-		})
-
-		t.Run("with an individual account", func(t *testing.T) {
-			testCase(t, individual)
-		})
-
-		t.Run("with an organization account", func(t *testing.T) {
-			testCase(t, organization)
-		})
-
-	})
+	// t.Run("can be configured to overwrite files on create", func(t *testing.T) {
+	//
+	// 	config := fmt.Sprintf(`
+	// 		resource "github_repository" "test" {
+	// 		  name      = "tf-acc-test-%s"
+	// 		  auto_init = true
+	// 		}
+	//
+	// 		resource "github_repository_file" "test" {
+	// 			repository          = github_repository.test.name
+	// 			branch              = "main"
+	// 			file                = "README.md"
+	// 			content             = "overwritten"
+	// 			overwrite_on_create = false
+	// 		}
+	//
+	// 	`, randomID)
+	//
+	// 	check := resource.ComposeTestCheckFunc(
+	// 		resource.TestCheckResourceAttr(
+	// 			"github_repository_file.test", "content",
+	// 			"overwritten",
+	// 		),
+	// 		resource.TestCheckResourceAttr(
+	// 			"github_repository_file.test", "sha",
+	// 			"67c1a95c2d9bb138aefeaebb319cca82e531736b",
+	// 		),
+	// 	)
+	//
+	// 	testCase := func(t *testing.T, mode string) {
+	// 		resource.Test(t, resource.TestCase{
+	// 			PreCheck:  func() { skipUnlessMode(t, mode) },
+	// 			Providers: testAccProviders,
+	// 			Steps: []resource.TestStep{
+	// 				{
+	// 					Config:      config,
+	// 					ExpectError: regexp.MustCompile(`Refusing to overwrite existing file`),
+	// 				},
+	// 				{
+	// 					Config: strings.Replace(config,
+	// 						"overwrite_on_create = false",
+	// 						"overwrite_on_create = true", 1),
+	// 					Check: check,
+	// 				},
+	// 			},
+	// 		})
+	// 	}
+	//
+	// 	t.Run("with an anonymous account", func(t *testing.T) {
+	// 		t.Skip("anonymous account not supported for this operation")
+	// 	})
+	//
+	// 	t.Run("with an individual account", func(t *testing.T) {
+	// 		testCase(t, individual)
+	// 	})
+	//
+	// 	t.Run("with an organization account", func(t *testing.T) {
+	// 		testCase(t, organization)
+	// 	})
+	//
+	// })
 }
