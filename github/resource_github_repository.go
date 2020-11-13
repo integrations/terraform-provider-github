303cc4b2543d310dafaa1915b16f1b7d4756b46c
Benjamin De Bernardi <debernardi.benjamin@gmail.com>
2020-11-13 15:46:07 +0100

    Keep visibility setting on repository creation

--- github/resource_github_repository.go
+++ github/resource_github_repository.go
@@ -223,11 +223,6 @@ func resourceGithubRepositoryCreate(d *schema.ResourceData, meta interface{}) er
 	repoReq := resourceGithubRepositoryObject(d)
 	owner := meta.(*Owner).name
 
-	// Auth issues (403 You need admin access to the organization before adding a repository to it.)
-	// are encountered when the resources is created with the visibility parameter. As
-	// resourceGithubRepositoryUpdate is called immediately after, this is subsequently corrected.
-	repoReq.Visibility = nil
-
 	repoName := repoReq.GetName()
 	ctx := context.Background()
 
