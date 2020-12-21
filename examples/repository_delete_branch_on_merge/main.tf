8b217197566807bc2f343cfa48caea3eb3adeb06
Ichinose Shogo <shogo82148@gmail.com>
2020-12-21 19:53:47 +0900

    change private to visibility

--- examples/repository_delete_branch_on_merge/main.tf
+++ examples/repository_delete_branch_on_merge/main.tf
@@ -2,6 +2,6 @@ resource "github_repository" "delete_branch_on_merge" {
   name                   = "delete_branch_on_merge"
   description            = "A repository with delete-branch-on-merge configured"
   default_branch         = "main"
-  private                = true
+  visibility             = "private"
   delete_branch_on_merge = true
 }
