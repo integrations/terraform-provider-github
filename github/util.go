97c79573fd87d370a057049652508ea5bef16e60
Kevin Zhao <kzhao@lyft.com>
2021-02-17 16:16:36 -0800

    Modify github_team_repository to accept slug as a valid team_id as well (#693)
    * First attempt
    
    * Add comment
    
    * Attempt to modify unit tests
    
    * Edit docs to reflect change
    
    * Make sure team_id is set appropriately
    
    * fixing lint
    
    * add passing tests for `github_team_repository`
    
    Co-authored-by: Jeremy Udit <jcudit@github.com>

--- github/util.go
+++ github/util.go
@@ -1,6 +1,8 @@
 package github
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"strconv"
 	"strings"
@@ -109,4 +111,35 @@ func validateTeamIDFunc(v interface{}, keyName string) (we []string, errors []er
 func splitRepoFilePath(path string) (string, string) {
 	parts := strings.Split(path, "/")
 	return parts[0], strings.Join(parts[1:], "/")
+}
+
+func getTeamID(teamIDString string, meta interface{}) (int64, error) {
+	// Given a string that is either a team id or team slug, return the
+	// id of the team it is referring to.
+	ctx := context.Background()
+	client := meta.(*Owner).v3client
+	orgName := meta.(*Owner).name
+	orgId := meta.(*Owner).id
+
+	teamId, parseIntErr := strconv.ParseInt(teamIDString, 10, 64)
+	if parseIntErr != nil {
+		// The given id not an integer, assume it is a team slug
+		team, _, slugErr := client.Teams.GetTeamBySlug(ctx, orgName, teamIDString)
+		if slugErr != nil {
+			return -1, errors.New(parseIntErr.Error() + slugErr.Error())
+		}
+		return team.GetID(), nil
+	} else {
+		// The given id is an integer, assume it is a team id
+		team, _, teamIdErr := client.Teams.GetTeamByID(ctx, orgId, teamId)
+		if teamIdErr != nil {
+			// There isn't a team with the given ID, assume it is a teamslug
+			team, _, slugErr := client.Teams.GetTeamBySlug(ctx, orgName, teamIDString)
+			if slugErr != nil {
+				return -1, errors.New(teamIdErr.Error() + slugErr.Error())
+			}
+			return team.GetID(), nil
+		}
+		return team.GetID(), nil
+	}
 }
