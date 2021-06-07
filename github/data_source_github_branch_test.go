6e88301571fc4ccbcdae48830ba0b28c035d9cd1
Naden <naden@mac.com>
2021-06-08 09:58:22 +1000

    Return nil instead of 404 error if branch does not exist (#806)

--- github/data_source_github_branch_test.go
+++ github/data_source_github_branch_test.go
@@ -59,4 +59,51 @@ func TestAccGithubBranchDataSource(t *testing.T) {
 		})
 
 	})
+
+	t.Run("queries an invalid branch without error", func(t *testing.T) {
+
+		config := fmt.Sprintf(`
+			resource "github_repository" "test" {
+			  name = "tf-acc-test-%[1]s"
+				auto_init = true
+			}
+
+			data "github_branch" "test" {
+				repository = github_repository.test.id
+				branch = "xxxxxx"
+			}
+		`, randomID)
+
+		check := resource.ComposeTestCheckFunc(
+			resource.TestCheckNoResourceAttr(
+				"data.github_branch.test", "id",
+			),
+		)
+
+		testCase := func(t *testing.T, mode string) {
+			resource.Test(t, resource.TestCase{
+				PreCheck:  func() { skipUnlessMode(t, mode) },
+				Providers: testAccProviders,
+				Steps: []resource.TestStep{
+					{
+						Config: config,
+						Check:  check,
+					},
+				},
+			})
+		}
+
+		t.Run("with an anonymous account", func(t *testing.T) {
+			t.Skip("anonymous account not supported for this operation")
+		})
+
+		t.Run("with an individual account", func(t *testing.T) {
+			testCase(t, individual)
+		})
+
+		t.Run("with an organization account", func(t *testing.T) {
+			testCase(t, organization)
+		})
+
+	})
 }
