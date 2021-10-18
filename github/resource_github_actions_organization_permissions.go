832a39781d670471387f2c0a070262ff30fddfd5
Joe H <34035447+j-nix@users.noreply.github.com>
2021-10-18 10:08:40 +0200

    Update github/resource_github_actions_organization_permissions.go
    Bump to latest version of github go-sdk
    
    Co-authored-by: Jeremy Udit <jcudit@github.com>

--- github/resource_github_actions_organization_permissions.go
+++ github/resource_github_actions_organization_permissions.go
@@ -5,7 +5,7 @@ import (
 	"errors"
 	"log"
 
-	"github.com/google/go-github/v38/github"
+	"github.com/google/go-github/v39/github"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 )
