c9b8d237cbcb0637c3fb57e008d6b51a9596934a
Samuel Ryan <sam@samryan.co.uk>
2021-10-31 12:47:05 +0000

    docs: Describe replacement of `\n` with new lines in PEM file

--- github/provider.go
+++ github/provider.go
@@ -218,6 +218,13 @@ func providerConfigure(p *schema.Provider) schema.ConfigureFunc {
 			}
 
 			if v, ok := appAuthAttr["pem_file"].(string); ok && v != "" {
+				// The Go encoding/pem package only decodes PEM formatted blocks
+				// that contain new lines. Some platforms, like Terraform Cloud,
+				// do not support new lines within Environment Variables.
+				// Any occurrence of \n in the `pem_file` argument's value 
+				// (explicit value, or default value taken from 
+				// GITHUB_APP_PEM_FILE Environment Variable) is replaced with an
+				// actual new line character before decoding.
 				appPemFile = strings.Replace(v, `\n`, "\n", -1)
 			} else {
 				return nil, fmt.Errorf("app_auth.pem_file must be set and contain a non-empty value")
