ec451315142567dfcf83e196e1e7a8978ecfddf2
Pascal Lalonde <pascal.lalonde@reddit.com>
2022-01-12 16:07:26 -0500

    Test for 'nil' on GetRequiredConversationResolution() to avoid crash.

--- github/resource_github_branch_protection_v3.go
+++ github/resource_github_branch_protection_v3.go
@@ -242,7 +242,9 @@ func resourceGithubBranchProtectionV3Read(d *schema.ResourceData, meta interface
 	d.Set("repository", repoName)
 	d.Set("branch", branch)
 	d.Set("enforce_admins", githubProtection.GetEnforceAdmins().Enabled)
-	d.Set("require_conversation_resolution", githubProtection.GetRequiredConversationResolution().Enabled)
+	if rcr := githubProtection.GetRequiredConversationResolution(); rcr != nil {
+		d.Set("require_conversation_resolution", rcr.Enabled)
+	}
 
 	if err := flattenAndSetRequiredStatusChecks(d, githubProtection); err != nil {
 		return fmt.Errorf("Error setting required_status_checks: %v", err)
