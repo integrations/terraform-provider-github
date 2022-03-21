5aec356ce451619347d1d39f8e79e4d33cca8085
Keegan Campbell <kfcampbell@github.com>
2022-03-21 15:57:56 -0700

    Whitespace change to test commit verification

--- github/resource_github_emu_group_mapping.go
+++ github/resource_github_emu_group_mapping.go
@@ -19,7 +19,6 @@ func resourceGithubEMUGroupMapping() *schema.Resource {
 			State: func(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
 				fmt.Printf("resource data: %v, meta: %v", d, meta)
 				d.Set("team_slug", d.Id())
-
 				orgName := meta.(*Owner).name
 				teamSlug, ok := d.Get("team_slug").(string)
 				if !ok {
