7ae7c17e31ec1ffe9987398b6afc01899387f814
joshuahancox <67631498+joshuahancox@users.noreply.github.com>
2022-04-12 13:53:50 +0100

    feat(github_team_repository): allow for custom repository roles

--- github/resource_github_team_repository.go
+++ github/resource_github_team_repository.go
@@ -36,7 +36,6 @@ func resourceGithubTeamRepository() *schema.Resource {
 				Type:         schema.TypeString,
 				Optional:     true,
 				Default:      "pull",
-				ValidateFunc: validateValueFunc([]string{"pull", "triage", "push", "maintain", "admin"}),
 			},
 			"etag": {
 				Type:     schema.TypeString,
