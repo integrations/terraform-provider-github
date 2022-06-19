52cfdb0f739787a49f5ed50b8d4ab798c75b7125
Blake Williams <code@shabbyrobe.org>
2022-06-20 00:24:42 +1000

    Add release assets

--- github/data_source_github_release.go
+++ github/data_source_github_release.go
@@ -94,6 +94,54 @@ func dataSourceGithubRelease() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+			"assets": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"url": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"node_id": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"label": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"content_type": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"size": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"created_at": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"updated_at": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"browser_download_url": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
 		},
 	}
 }
@@ -148,6 +196,28 @@ func dataSourceGithubReleaseRead(d *schema.ResourceData, meta interface{}) error
 	d.Set("upload_url", release.GetUploadURL())
 	d.Set("zipball_url", release.GetZipballURL())
 	d.Set("tarball_url", release.GetTarballURL())
+
+	assets := make([]interface{}, 0, len(release.Assets))
+	for _, releaseAsset := range release.Assets {
+		if releaseAsset == nil {
+			continue
+		}
+
+		assets = append(assets, map[string]interface{}{
+			"id":                   releaseAsset.GetID(),
+			"url":                  releaseAsset.GetURL(),
+			"node_id":              releaseAsset.GetNodeID(),
+			"name":                 releaseAsset.GetName(),
+			"label":                releaseAsset.GetLabel(),
+			"content_type":         releaseAsset.GetContentType(),
+			"size":                 releaseAsset.GetSize(),
+			"created_at":           releaseAsset.GetCreatedAt(),
+			"updated_at":           releaseAsset.GetUpdatedAt(),
+			"browser_download_url": releaseAsset.GetBrowserDownloadURL(),
+		})
+	}
+
+	d.Set("assets", assets)
 
 	return nil
 }
