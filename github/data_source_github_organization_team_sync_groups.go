fee67673386da2e8dde618238eb9bf844bfe56f7
Keegan Campbell <me@kfcampbell.com>
2022-08-30 13:01:46 -0700

    Bump go-github to v47

--- github/data_source_github_organization_team_sync_groups.go
+++ github/data_source_github_organization_team_sync_groups.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/google/go-github/v46/github"
+	"github.com/google/go-github/v47/github"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
--- github/resource_github_branch_default.go
+++ github/resource_github_branch_default.go
@@ -3,7 +3,7 @@ package github
 import (
 	"context"
 
-	"github.com/google/go-github/v46/github"
+	"github.com/google/go-github/v47/github"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
