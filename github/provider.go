0080e4a3a93f9166233565eac63c26cd8a2fd970
Bertrand Paquet <bertrand.paquet@gmail.com>
2022-10-14 01:01:45 +0200

    Add datasource: github_repository_deploy_keys (#1299)
    * Add datasource: github_repository_deploy_keys
    
    * Implement pagination
    
    * Add missing link into the main doc

--- github/provider.go
+++ github/provider.go
@@ -155,6 +155,7 @@ func Provider() terraform.ResourceProvider {
 			"github_repositories":                    dataSourceGithubRepositories(),
 			"github_repository":                      dataSourceGithubRepository(),
 			"github_repository_branches":             dataSourceGithubRepositoryBranches(),
+			"github_repository_deploy_keys":          dataSourceGithubRepositoryDeployKeys(),
 			"github_repository_file":                 dataSourceGithubRepositoryFile(),
 			"github_repository_milestone":            dataSourceGithubRepositoryMilestone(),
 			"github_repository_pull_request":         dataSourceGithubRepositoryPullRequest(),
