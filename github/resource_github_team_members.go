670e6e97fed2f240c25cfe432ea9a87a66ff63ff
Deniss Lapcneko <dennis.lapchenko@gmail.com>
2023-02-10 13:27:31 +0800

    feat(team_members): allow team id lookup from slug

--- github/resource_github_team_members.go
+++ github/resource_github_team_members.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"log"
 	"reflect"
-	"strconv"
 
 	"github.com/google/go-github/v50/github"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
@@ -64,9 +63,9 @@ func resourceGithubTeamMembersCreate(d *schema.ResourceData, meta interface{}) e
 	orgId := meta.(*Owner).id
 
 	teamIdString := d.Get("team_id").(string)
-	teamId, err := strconv.ParseInt(teamIdString, 10, 64)
+	teamId, err := getTeamID(teamIdString, meta)
 	if err != nil {
-		return unconvertibleIdErr(teamIdString, err)
+		return err
 	}
 	ctx := context.Background()
 
@@ -100,9 +99,9 @@ func resourceGithubTeamMembersUpdate(d *schema.ResourceData, meta interface{}) e
 	orgId := meta.(*Owner).id
 
 	teamIdString := d.Get("team_id").(string)
-	teamId, err := strconv.ParseInt(teamIdString, 10, 64)
+	teamId, err := getTeamID(teamIdString, meta)
 	if err != nil {
-		return unconvertibleIdErr(teamIdString, err)
+		return err
 	}
 	ctx := context.Background()
 
@@ -182,9 +181,9 @@ func resourceGithubTeamMembersRead(d *schema.ResourceData, meta interface{}) err
 		teamIdString = d.Id()
 	}
 
-	teamId, err := strconv.ParseInt(teamIdString, 10, 64)
+	teamId, err := getTeamID(teamIdString, meta)
 	if err != nil {
-		return unconvertibleIdErr(teamIdString, err)
+		return err
 	}
 
 	// We intentionally set these early to allow reconciliation
@@ -275,9 +274,9 @@ func resourceGithubTeamMembersDelete(d *schema.ResourceData, meta interface{}) e
 	client := meta.(*Owner).v3client
 	orgId := meta.(*Owner).id
 	teamIdString := d.Get("team_id").(string)
-	teamId, err := strconv.ParseInt(teamIdString, 10, 64)
+	teamId, err := getTeamID(teamIdString, meta)
 	if err != nil {
-		return unconvertibleIdErr(teamIdString, err)
+		return err
 	}
 
 	members := d.Get("members").(*schema.Set)
