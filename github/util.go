941c1dc9d83fa01f1f888fe30fb3874dfb8d31af
Aniruddha Maru <aniruddhamaru@gmail.com>
2023-03-04 16:55:54 -0800

    more v50

--- github/util.go
+++ github/util.go
@@ -12,7 +12,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/google/go-github/v48/github"
+	"github.com/google/go-github/v50/github"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
