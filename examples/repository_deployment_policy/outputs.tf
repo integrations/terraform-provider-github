fc1a22b5a6a66c712fbd44056036f207dacde472
Massimiliano Donini <massimiliano.donini@gmail.com>
2023-03-17 19:36:03 +0100

    Undo example add

--- examples/repository_deployment_policy/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "branch_protection_policy" {
-  description = "Example repository JSON blob"
-  value       = github_repository_environment_deployment_policy.terraformed
-}
--- examples/repository_deployment_policy/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "owner" {
-  description = "GitHub owner used to configure the provider"
-  type        = string
-}
-
-variable "github_token" {
-  description = "GitHub access token used to configure the provider"
-  type        = string
-}
