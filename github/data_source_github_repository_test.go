df07cd33011924667c8668d270294eca1b6a4658
Isaac Sanders <651597+isaacsanders@users.noreply.github.com>
2023-03-23 17:38:29 -0500

    fix: Removes pages from repository tests

--- github/data_source_github_repository_test.go
+++ github/data_source_github_repository_test.go
@@ -51,60 +51,6 @@ func TestAccGithubRepositoryDataSource(t *testing.T) {
 
 	})
 
-	t.Run("queries a repository with pages configured", func(t *testing.T) {
-
-		randomID := acctest.RandStringFromCharSet(5, acctest.CharSetAlphaNum)
-
-		config := fmt.Sprintf(`
-			resource "github_repository" "test" {
-				name         = "tf-acc-%s"
-				auto_init    = true
-				pages {
-					source {
-						branch = "main"
-					}
-				}
-			}
-
-			data "github_repository" "test" {
-				name = github_repository.test.name
-			}
-		`, randomID)
-
-		check := resource.ComposeTestCheckFunc(
-			resource.TestCheckResourceAttr(
-				"data.github_repository.test", "pages.0.source.0.branch",
-				"main",
-			),
-		)
-
-		testCase := func(t *testing.T, mode string) {
-			resource.Test(t, resource.TestCase{
-				PreCheck:  func() { skipUnlessMode(t, mode) },
-				Providers: testAccProviders,
-				Steps: []resource.TestStep{
-					{
-						Config: config,
-						Check:  check,
-					},
-				},
-			})
-		}
-
-		t.Run("with an anonymous account", func(t *testing.T) {
-			t.Skip("anonymous account not supported for this operation")
-		})
-
-		t.Run("with an individual account", func(t *testing.T) {
-			testCase(t, individual)
-		})
-
-		t.Run("with an organization account", func(t *testing.T) {
-			testCase(t, organization)
-		})
-
-	})
-
 	t.Run("checks defaults on a new repository", func(t *testing.T) {
 
 		randomID := acctest.RandStringFromCharSet(5, acctest.CharSetAlphaNum)
@@ -133,9 +79,6 @@ func TestAccGithubRepositoryDataSource(t *testing.T) {
 			),
 			resource.TestCheckResourceAttr(
 				"data.github_repository.test", "homepage_url", "",
-			),
-			resource.TestCheckResourceAttr(
-				"data.github_repository.test", "pages.#", "0",
 			),
 		)
 
