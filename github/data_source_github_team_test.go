312f54ca9500536db04b9bb85858b0d57778ad1e
felixlut <felix.luthman@gmail.com>
2023-05-06 23:17:08 +0200

    fix tests

--- github/data_source_github_team_test.go
+++ github/data_source_github_team_test.go
@@ -240,39 +240,18 @@ func TestAccGithubTeamDataSource(t *testing.T) {
 			resource "github_team_repository" "test" {
 				team_id    = github_team.test.id
 				repository = github_repository.test.name
-				permission = "pull"
-			}
-			data "github_repository" "test" {
-				name = github_repository.test.name
-				depends_on = [
-					github_team.test,
-					github_repository.test,
-					github_team_repository.test
-				]
-			}
-			data "github_repository_teams" "test" {
-				name = github_repository.test.name
-				depends_on = [
-					github_team.test,
-					github_repository.test,
-					github_team_repository.test
-				]
+				permission = "admin"
 			}
 			data "github_team" "test" {
 				slug = github_team.test.slug
-				depends_on = [
-					github_team.test,
-					github_repository.test,
-					github_team_repository.test
-				]
 			}
 		`, randomID)
 
 		check := resource.ComposeAggregateTestCheckFunc(
 			resource.TestCheckResourceAttrSet("data.github_team.test", "name"),
 			resource.TestCheckResourceAttr("data.github_team.test", "repositories_detailed.#", "1"),
-			resource.TestCheckResourceAttrPair("data.github_team.test", "repositories_detailed.0.repo_id", "data.github_repository.test", "repo_id"),
-			resource.TestCheckResourceAttrPair("data.github_team.test", "repositories_detailed.0.role_name", "data.github_repository_teams.test", "teams.0.permission"),
+			resource.TestCheckResourceAttrPair("data.github_team.test", "repositories_detailed.0.repo_id", "github_repository.test", "repo_id"),
+			resource.TestCheckResourceAttrPair("data.github_team.test", "repositories_detailed.0.role_name", "github_team_repository.test", "permission"),
 		)
 
 		testCase := func(t *testing.T, mode string) {
