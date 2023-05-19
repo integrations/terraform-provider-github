af545ec5588a5dea47f267de9cdbd362481d0086
Bertrand Paquet <bertrand.paquet@gmail.com>
2023-05-19 21:03:29 +0200

    Add data source: github_repository_autolink_references

---
+++ github/data_source_github_repository_autolink_references_test.go
@@ -0,0 +1,72 @@
+package github
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccGithubRepositoryAutolinkReferencesDataSource(t *testing.T) {
+
+	randomID := acctest.RandStringFromCharSet(5, acctest.CharSetAlphaNum)
+
+	t.Run("queries autolink references", func(t *testing.T) {
+
+		config := fmt.Sprintf(`
+			resource "github_repository" "test" {
+			  name = "tf-acc-test-%[1]s"
+				auto_init = true
+			}
+
+			resource "github_repository_autolink_reference" "autolink_default" {
+				repository = github_repository.test.name
+
+				key_prefix          = "TEST1-"
+				target_url_template = "https://example.com/TEST-<num>"
+			}
+		`, randomID)
+
+		config2 := config + `
+			data "github_repository_autolink_references" "all" {
+				repository = github_repository.test.name
+			}
+		`
+		check := resource.ComposeTestCheckFunc(
+			resource.TestCheckResourceAttr("data.github_repository_autolink_references.all", "autolink_references.#", "1"),
+			resource.TestCheckResourceAttr("data.github_repository_autolink_references.all", "autolink_references.0.key_prefix", "TEST1-"),
+			resource.TestCheckResourceAttr("data.github_repository_autolink_references.all", "autolink_references.0.target_url_template", "https://example.com/TEST-<num>"),
+			resource.TestCheckResourceAttr("data.github_repository_autolink_references.all", "autolink_references.0.is_alphanumeric", "true"),
+		)
+
+		testCase := func(t *testing.T, mode string) {
+			resource.Test(t, resource.TestCase{
+				PreCheck:  func() { skipUnlessMode(t, mode) },
+				Providers: testAccProviders,
+				Steps: []resource.TestStep{
+					{
+						Config: config,
+					},
+					{
+						Config: config2,
+						Check:  check,
+					},
+				},
+			})
+		}
+
+		t.Run("with an anonymous account", func(t *testing.T) {
+			t.Skip("anonymous account not supported for this operation")
+		})
+
+		t.Run("with an individual account", func(t *testing.T) {
+			testCase(t, individual)
+		})
+
+		t.Run("with an organization account", func(t *testing.T) {
+			testCase(t, organization)
+		})
+
+	})
+}
