62a966fe163207e7add9c10515645ba9fd0777b8
KenSpur <spur.ken@justbeawesome.net>
2023-06-21 15:33:24 +0200

    add codespaces_*public_key data source

---
+++ github/data_source_github_codespaces_user_public_key_test.go
@@ -0,0 +1,49 @@
+package github
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccGithubCodespacesUserPublicKeyDataSource(t *testing.T) {
+
+	t.Run("queries an user public key without error", func(t *testing.T) {
+
+		config := `
+			data "github_codespaces_user_public_key" "test" {}
+		`
+
+		check := resource.ComposeTestCheckFunc(
+			resource.TestCheckResourceAttrSet(
+				"data.github_codespaces_user_public_key.test", "key",
+			),
+		)
+
+		testCase := func(t *testing.T, mode string) {
+			resource.Test(t, resource.TestCase{
+				PreCheck:  func() { skipUnlessMode(t, mode) },
+				Providers: testAccProviders,
+				Steps: []resource.TestStep{
+					{
+						Config: config,
+						Check:  check,
+					},
+				},
+			})
+		}
+
+		t.Run("with an anonymous account", func(t *testing.T) {
+			t.Skip("anonymous account not supported for this operation")
+		})
+
+		t.Run("with an organization account", func(t *testing.T) {
+			t.Skip("organization account not supported for this operation")
+		})
+
+		t.Run("with an individual account", func(t *testing.T) {
+			testCase(t, individual)
+		})
+
+	})
+}
