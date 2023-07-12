e1f7240dbda84ba5dccac349e52feca75ba2fb64
Keegan Campbell <me@kfcampbell.com>
2023-07-12 15:58:58 -0700

    Remove unnecessary MaxItems on computed value

--- github/data_source_github_repository.go
+++ github/data_source_github_repository.go
@@ -190,7 +190,6 @@ func dataSourceGithubRepository() *schema.Resource {
 			"template": {
 				Type:     schema.TypeList,
 				Computed: true,
-				MaxItems: 1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"owner": {
