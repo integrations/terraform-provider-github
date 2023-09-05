b61a3f3fd741852bee447ea8a49ca7d2a57edf92
Osama Faqhruldin <onfaqhru@gmail.com>
2023-09-05 09:00:14 -0400

    Check repository_deployment_branch_policy for 404 in read

--- github/resource_github_repository_deployment_branch_policy.go
+++ github/resource_github_repository_deployment_branch_policy.go
@@ -2,6 +2,8 @@ package github
 
 import (
 	"context"
+	"log"
+	"net/http"
 	"strconv"
 
 	"github.com/google/go-github/v54/github"
@@ -105,6 +107,20 @@ func resourceGithubRepositoryDeploymentBranchPolicyRead(d *schema.ResourceData, 
 	}
 
 	policy, resp, err := client.Repositories.GetDeploymentBranchPolicy(ctx, owner, repoName, environmentName, int64(id))
+	if err != nil {
+		if ghErr, ok := err.(*github.ErrorResponse); ok {
+			if ghErr.Response.StatusCode == http.StatusNotModified {
+				return nil
+			}
+			if ghErr.Response.StatusCode == http.StatusNotFound {
+				log.Printf("[INFO] Removing deployment branch policy for environment %s: %s from state because it no longer exists in GitHub",
+					repoName, environmentName)
+				d.SetId("")
+				return nil
+			}
+		}
+		return err
+	}
 	if err != nil && resp.StatusCode == 304 {
 		return nil
 	}
