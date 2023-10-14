6747dac9a1d26678994e97639c203008431b1e2e
Nikolai Mishin <sanduku.default@gmail.com>
2023-10-14 22:13:55 +0200

    Add support for packages IP ranges in github_ip_ranges

--- github/data_source_github_ip_ranges.go
+++ github/data_source_github_ip_ranges.go
@@ -32,6 +32,11 @@ func dataSourceGithubIpRanges() *schema.Resource {
 				Computed: true,
 				Elem:     &schema.Schema{Type: schema.TypeString},
 			},
+                        "packages": {
+                                Type:     schema.TypeList,
+                                Computed: true,
+                                Elem:     &schema.Schema{Type: schema.TypeString},
+                        },
 			"pages": {
 				Type:     schema.TypeList,
 				Computed: true,
@@ -72,6 +77,11 @@ func dataSourceGithubIpRanges() *schema.Resource {
 				Computed: true,
 				Elem:     &schema.Schema{Type: schema.TypeString},
 			},
+                        "packages_ipv4": {
+                                Type:     schema.TypeList,
+                                Computed: true,
+                                Elem:     &schema.Schema{Type: schema.TypeString},
+                        },
 			"pages_ipv4": {
 				Type:     schema.TypeList,
 				Computed: true,
@@ -112,6 +122,11 @@ func dataSourceGithubIpRanges() *schema.Resource {
 				Computed: true,
 				Elem:     &schema.Schema{Type: schema.TypeString},
 			},
+                        "packages_ipv6": {
+                                Type:     schema.TypeList,
+                                Computed: true,
+                                Elem:     &schema.Schema{Type: schema.TypeString},
+                        },
 			"pages_ipv6": {
 				Type:     schema.TypeList,
 				Computed: true,
@@ -153,6 +168,11 @@ func dataSourceGithubIpRangesRead(d *schema.ResourceData, meta interface{}) erro
 	if err != nil {
 		return err
 	}
+
+        cidrPackagesIpv4, cidrPackagesIpv6, err := splitIpv4Ipv6Cidrs(&api.Packages)
+        if err != nil {
+                return err
+        }
 
 	cidrPagesIpv4, cidrPagesIpv6, err := splitIpv4Ipv6Cidrs(&api.Pages)
 	if err != nil {
@@ -197,6 +217,11 @@ func dataSourceGithubIpRangesRead(d *schema.ResourceData, meta interface{}) erro
 		d.Set("git_ipv4", cidrGitIpv4)
 		d.Set("git_ipv6", cidrGitIpv6)
 	}
+        if len(api.Packages) > 0 {
+                d.Set("packages", api.Packages)
+                d.Set("packages_ipv4", cidrPackagesIpv4)
+                d.Set("packages_ipv6", cidrPackagesIpv6)
+        }
 	if len(api.Pages) > 0 {
 		d.Set("pages", api.Pages)
 		d.Set("pages_ipv4", cidrPagesIpv4)
