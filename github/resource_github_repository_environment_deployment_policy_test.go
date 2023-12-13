9b44b636b953fdb86f094a29ef3dc379a4ae5777
Peter McEvoy <git@mcevoypeter.com>
2023-12-13 13:17:19 -0700

    Revert "Add tag-based test for github_repository_environment_deployment_policy"
    This reverts commit 88b13690b22c6dd9e2decc258d3e235b03a6d5cc.

--- github/resource_github_repository_environment_deployment_policy_test.go
+++ github/resource_github_repository_environment_deployment_policy_test.go
@@ -8,11 +8,11 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func TestAccGithubRepositoryEnvironmentDeploymentPolicyBranch(t *testing.T) {
+func TestAccGithubRepositoryEnvironmentDeploymentPolicy(t *testing.T) {
 
 	randomID := acctest.RandStringFromCharSet(5, acctest.CharSetAlphaNum)
 
-	t.Run("creates a repository environment with branch-based deployment policy", func(t *testing.T) {
+	t.Run("creates a repository environment with deployment policy", func(t *testing.T) {
 
 		config := fmt.Sprintf(`
 
@@ -62,91 +62,6 @@ func TestAccGithubRepositoryEnvironmentDeploymentPolicyBranch(t *testing.T) {
 			resource.TestCheckResourceAttr(
 				"github_repository_environment_deployment_policy.test", "type",
 				"branch",
-			),
-		)
-
-		testCase := func(t *testing.T, mode string) {
-			resource.Test(t, resource.TestCase{
-				PreCheck:  func() { skipUnlessMode(t, mode) },
-				Providers: testAccProviders,
-				Steps: []resource.TestStep{
-					{
-						Config: config,
-						Check:  check,
-					},
-				},
-			})
-		}
-
-		t.Run("with an anonymous account", func(t *testing.T) {
-			t.Skip("anonymous account not supported for this operation")
-		})
-
-		t.Run("with an individual account", func(t *testing.T) {
-			testCase(t, individual)
-		})
-
-		t.Run("with an organization account", func(t *testing.T) {
-			testCase(t, organization)
-		})
-
-	})
-}
-
-func TestAccGithubRepositoryEnvironmentDeploymentPolicyTag(t *testing.T) {
-
-	randomID := acctest.RandStringFromCharSet(5, acctest.CharSetAlphaNum)
-
-	t.Run("creates a repository environment with tag-based deployment policy", func(t *testing.T) {
-
-		config := fmt.Sprintf(`
-
-			data "github_user" "current" {
-				username = ""
-			}
-
-			resource "github_repository" "test" {
-				name      = "tf-acc-test-%s"
-			}
-
-			resource "github_repository_environment" "test" {
-				repository  = github_repository.test.name
-				environment = "environment/test"
-				wait_timer  = 10000
-				reviewers {
-					users = [data.github_user.current.id]
-				}
-				deployment_branch_policy {
-					protected_branches     = false
-					custom_branch_policies = true
-				}
-			}
-
-			resource "github_repository_environment_deployment_policy" "test" {
-				repository  = github_repository.test.name
-				environment = github_repository_environment.test.environment
-				pattern     = "v*"
-				type        = "tag"
-			}
-
-		`, randomID)
-
-		check := resource.ComposeTestCheckFunc(
-			resource.TestCheckResourceAttr(
-				"github_repository_environment_deployment_policy.test", "repository",
-				fmt.Sprintf("tf-acc-test-%s", randomID),
-			),
-			resource.TestCheckResourceAttr(
-				"github_repository_environment_deployment_policy.test", "environment",
-				"environment/test",
-			),
-			resource.TestCheckResourceAttr(
-				"github_repository_environment_deployment_policy.test", "pattern",
-				"v*",
-			),
-			resource.TestCheckResourceAttr(
-				"github_repository_environment_deployment_policy.test", "type",
-				"tag",
 			),
 		)
 
