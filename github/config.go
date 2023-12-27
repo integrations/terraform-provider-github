integrations/terraform-provider-github

commit 17411330b92e97cc04d7c03a3856ff3a4a423900

Initial commit of allowing owners for anonymous auth (#1029)

github/config.go
@@ -2,7 +2,6 @@ package github
 
 import (
 	"context"
-	"log"
 	"net/http"
 	"net/url"
 	"path"
@@ -152,17 +151,11 @@ func (c *Config) Meta() (interface{}, error) {
 	owner.v4client = v4client
 	owner.v3client = v3client
 
-	if c.Anonymous() {
-		log.Printf("[INFO] No token present; configuring anonymous owner.")
-		return &owner, nil
-	} else {
-		_, err = c.ConfigureOwner(&owner)
-		if err != nil {
-			return &owner, err
-		}
-		log.Printf("[INFO] Token present; configuring authenticated owner: %s", owner.name)
-		return &owner, nil
+	_, err = c.ConfigureOwner(&owner)
+	if err != nil {
+		return &owner, err
 	}
+	return &owner, nil
 }
 
 type previewHeaderInjectorTransport struct {

commit e8d556463b18033ab230542db2649d79834321d7

Avoid getting user when anonymous
Resolves regressions #2008 and #2014 introduced in #1029

Signed-off-by: Marco Nenciarini <mnencia@kcore.it>

github/config.go
@@ -107,6 +107,9 @@ func (c *Config) ConfigureOwner(owner *Owner) (*Owner, error) {
 	ctx := context.Background()
 	owner.name = c.Owner
 	if owner.name == "" {
+		if c.Anonymous() {
+			return owner, nil
+		}
 		// Discover authenticated user
 		user, _, err := owner.v3client.Users.Get(ctx, "")
 		if err != nil {