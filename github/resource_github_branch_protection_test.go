393bf881051926271dc4ed5f398c207dc0be8f20
Keegan Campbell <me@kfcampbell.com>
2024-01-22 13:23:41 -0800

    Pass context.Background() in test

--- github/resource_github_branch_protection_test.go
+++ github/resource_github_branch_protection_test.go
@@ -1,6 +1,7 @@
 package github
 
 import (
+	"context"
 	"fmt"
 	"reflect"
 	"regexp"
@@ -912,7 +913,7 @@ func testGithubBranchProtectionStateDataV2() map[string]interface{} {
 
 func TestAccGithubBranchProtectionV4StateUpgradeV1(t *testing.T) {
 	expected := testGithubBranchProtectionStateDataV2()
-	actual, err := resourceGithubBranchProtectionUpgradeV1(nil, testGithubBranchProtectionStateDataV1(), nil)
+	actual, err := resourceGithubBranchProtectionUpgradeV1(context.Background(), testGithubBranchProtectionStateDataV1(), nil)
 	if err != nil {
 		t.Fatalf("error migrating state: %s", err)
 	}
