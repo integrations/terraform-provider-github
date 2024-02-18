c5655b71a511eab4329f8dd0f36dd8ce5b72a104
felixlut <felix.luthman@snowsoftware.com>
2024-02-18 17:55:43 +0100

    fix ordering of imports

--- github/data_source_github_organization_test.go
+++ github/data_source_github_organization_test.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccGithubOrganizationDataSource(t *testing.T) {
