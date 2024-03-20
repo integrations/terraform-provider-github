99db17211573238ab3fde79e3477f41657da26e9
Nick Floyd <139819+nickfloyd@users.noreply.github.com>
2024-03-20 15:04:16 -0500

    updates the env setup for optional env vars

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check for '-include-optional-env' parameter
+INCLUDE_OPTIONAL=false
+if [ "$1" == "-include-optional-env" ]; then
+    INCLUDE_OPTIONAL=true
+fi
+
 # Check if launch.json already exists
 if [ -f ".vscode/launch.json" ]; then
     read -p "launch.json already exists. Overwrite? (y/n): " overwrite
@@ -9,35 +15,41 @@ if [ -f ".vscode/launch.json" ]; then
     fi
 fi
 
-# Prompt for environment variables
-read -p "Enter TF_TEST_FILE: " TF_TEST_FILE
-read -p "Enter TF_TEST_FUNCTION: " TF_TEST_FUNCTION
-read -p "Enter GITHUB_TEST_COLLABORATOR: " GITHUB_TEST_COLLABORATOR
-read -p "Enter GITHUB_TEST_COLLABORATOR_TOKEN: " GITHUB_TEST_COLLABORATOR_TOKEN
-read -p "Enter GITHUB_TEST_USER: " GITHUB_TEST_USER
-read -p "Enter GITHUB_TOKEN: " GITHUB_TOKEN
-read -p "Enter GITHUB_TEMPLATE_REPOSITORY: " GITHUB_TEMPLATE_REPOSITORY
-read -p "Enter GITHUB_TEMPLATE_REPOSITORY_RELEASE_ID: " GITHUB_TEMPLATE_REPOSITORY_RELEASE_ID
-read -p "Enter GITHUB_ORGANIZATION: " GITHUB_ORGANIZATION
-read -p "Enter TF_CLI_CONFIG_FILE: " TF_CLI_CONFIG_FILE
-read -p "Enter TF_ACC: " TF_ACC
-read -p "Enter TF_LOG: " TF_LOG
-read -p "Enter APP_INSTALLATION_ID: " APP_INSTALLATION_ID
+# Prompt for required environment variables
 
-# Export environment variables
+read -p "Enter GITHUB_TOKEN: " GITHUB_TOKEN
+read -p "Enter GITHUB_OWNER: " GITHUB_OWNER
+read -p "Enter GITHUB_ORGANIZATION (ex. octokit): " GITHUB_ORGANIZATION
+read -p "Enter TF_TEST_FILE (ex. resource_github_team_members_test.go): " TF_TEST_FILE
+read -p "Enter TF_TEST_FUNCTION (ex. TestAccGitHubRepositoryTopics): " TF_TEST_FUNCTION
+
+export GITHUB_TOKEN="$GITHUB_TOKEN"
+export GITHUB_OWNER="$GITHUB_OWNER"
+export GITHUB_ORGANIZATION="$GITHUB_ORGANIZATION"
 export TF_TEST_FILE="$TF_TEST_FILE"
 export TF_TEST_FUNCTION="$TF_TEST_FUNCTION"
-export GITHUB_TEST_COLLABORATOR="$GITHUB_TEST_COLLABORATOR"
-export GITHUB_TEST_COLLABORATOR_TOKEN="$GITHUB_TEST_COLLABORATOR_TOKEN"
-export GITHUB_TEST_USER="$GITHUB_TEST_USER"
-export GITHUB_TOKEN="$GITHUB_TOKEN"
-export GITHUB_TEMPLATE_REPOSITORY="$GITHUB_TEMPLATE_REPOSITORY"
-export GITHUB_TEMPLATE_REPOSITORY_RELEASE_ID="$GITHUB_TEMPLATE_REPOSITORY_RELEASE_ID"
-export GITHUB_ORGANIZATION="$GITHUB_ORGANIZATION"
-export TF_CLI_CONFIG_FILE="$TF_CLI_CONFIG_FILE"
-export TF_ACC="$TF_ACC"
-export TF_LOG="$TF_LOG"
-export APP_INSTALLATION_ID="$APP_INSTALLATION_ID"
+
+# Prompt for optional environment variables
+if [ "$INCLUDE_OPTIONAL" = true ]; then
+  read -p "Enter GITHUB_TEST_USER: " GITHUB_TEST_USER
+  read -p "Enter GITHUB_TEST_COLLABORATOR: " GITHUB_TEST_COLLABORATOR
+  read -p "Enter GITHUB_TEST_COLLABORATOR_TOKEN: " GITHUB_TEST_COLLABORATOR_TOKEN
+  read -p "Enter GITHUB_TEMPLATE_REPOSITORY: " GITHUB_TEMPLATE_REPOSITORY
+  read -p "Enter GITHUB_TEMPLATE_REPOSITORY_RELEASE_ID: " GITHUB_TEMPLATE_REPOSITORY_RELEASE_ID
+  read -p "Enter TF_ACC: " TF_ACC
+  read -p "Enter APP_INSTALLATION_ID: " APP_INSTALLATION_ID
+
+  # Export environment variables
+  export GITHUB_TEST_USER="$GITHUB_TEST_USER"
+  export GITHUB_TEST_COLLABORATOR="$GITHUB_TEST_COLLABORATOR"
+  export GITHUB_TEST_COLLABORATOR_TOKEN="$GITHUB_TEST_COLLABORATOR_TOKEN"
+  export GITHUB_TEMPLATE_REPOSITORY="$GITHUB_TEMPLATE_REPOSITORY"
+  export GITHUB_TEMPLATE_REPOSITORY_RELEASE_ID="$GITHUB_TEMPLATE_REPOSITORY_RELEASE_ID"
+  export GITHUB_ORGANIZATION="$GITHUB_ORGANIZATION"
+  export TF_ACC="$TF_ACC"
+  export TF_LOG="DEBUG"
+  export APP_INSTALLATION_ID="$APP_INSTALLATION_ID"
+fi
 
 # Create the launch.json file
 cat << EOF > .vscode/launch.json
@@ -53,7 +65,7 @@ cat << EOF > .vscode/launch.json
       "args": [
         "-test.v",
         "-test.run",
-        "^\${env:TF_TEST_FILE}$"
+        "^\${env:TF_TEST_FUNCTION}$"
       ],
       "env": {
         "GITHUB_TEST_COLLABORATOR": "\${env:GITHUB_TEST_COLLABORATOR}",
@@ -63,7 +75,7 @@ cat << EOF > .vscode/launch.json
         "GITHUB_TEMPLATE_REPOSITORY": "\${env:GITHUB_TEMPLATE_REPOSITORY}",
         "GITHUB_TEMPLATE_REPOSITORY_RELEASE_ID": "\${env:GITHUB_TEMPLATE_REPOSITORY_RELEASE_ID}",
         "GITHUB_ORGANIZATION": "\${env:GITHUB_ORGANIZATION}",
-        "TF_CLI_CONFIG_FILE": "\${env:TF_CLI_CONFIG_FILE}",
+        "TF_CLI_CONFIG_FILE": "\${workspaceFolder}/examples/dev.tfrc",
         "TF_ACC": "\${env:TF_ACC}",
         "TF_LOG": "\${env:TF_LOG}",
         "APP_INSTALLATION_ID": "\${env:APP_INSTALLATION_ID}"
