182f8e082821862af3ee59e39a428efbd089cd50
Ali Reza <alirezayahya@gmail.com>
2024-08-06 20:28:16 +0200

    feat: add delete branch on merge option on datasource github repository (#2046)
    * add delete branch on merge option on datasource github repository
    
    * add allow update branch option as well
    
    * Add allow_update_branch and delete_branch_on_merge attributes to github_repository test
    
    ---------
    
    Co-authored-by: Nick Floyd <139819+nickfloyd@users.noreply.github.com>

--- github/data_source_github_repository.go
+++ github/data_source_github_repository.go
@@ -90,6 +90,10 @@ func dataSourceGithubRepository() *schema.Resource {
 				Type:     schema.TypeBool,
 				Computed: true,
 			},
+			"allow_update_branch": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
 			"squash_merge_commit_title": {
 				Type:     schema.TypeString,
 				Computed: true,
@@ -326,6 +330,10 @@ func dataSourceGithubRepository() *schema.Resource {
 				Type:     schema.TypeInt,
 				Computed: true,
 			},
+			"delete_branch_on_merge": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
 		},
 	}
 }
@@ -395,6 +403,8 @@ func dataSourceGithubRepositoryRead(d *schema.ResourceData, meta interface{}) er
 	d.Set("node_id", repo.GetNodeID())
 	d.Set("repo_id", repo.GetID())
 	d.Set("has_projects", repo.GetHasProjects())
+	d.Set("delete_branch_on_merge", repo.GetDeleteBranchOnMerge())
+	d.Set("allow_update_branch", repo.GetAllowUpdateBranch())
 
 	if repo.GetHasPages() {
 		pages, _, err := client.Repositories.GetPagesInfo(context.TODO(), owner, repoName)
