37044e8789a32c8689e730dce1c8762a5e1b8f81
joshhunt <git@joshhunt.dev>
2025-09-30 14:46:01 +0100

    Add 'exempt' bypass type

--- github/resource_github_repository_ruleset.go
+++ github/resource_github_repository_ruleset.go
@@ -69,7 +69,7 @@ func resourceGithubRepositoryRuleset() *schema.Resource {
 						"bypass_mode": {
 							Type:         schema.TypeString,
 							Required:     true,
-							ValidateFunc: validation.StringInSlice([]string{"always", "pull_request"}, false),
+							ValidateFunc: validation.StringInSlice([]string{"always", "pull_request", "exempt"}, false),
 							Description:  "When the specified actor can bypass the ruleset. pull_request means that an actor can only bypass rules on pull requests. Can be one of: `always`, `pull_request`.",
 						},
 					}},
