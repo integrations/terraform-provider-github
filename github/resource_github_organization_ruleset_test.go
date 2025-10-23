c126771d39d7243e837ce3ef5e369a4cfd87a85d
Nick Floyd <139819+nickfloyd@users.noreply.github.com>
2025-10-23 12:25:48 -0500

    Apply suggestion from @nickfloyd

--- github/resource_github_organization_ruleset_test.go
+++ github/resource_github_organization_ruleset_test.go
@@ -61,7 +61,6 @@ func TestGithubOrganizationRulesets(t *testing.T) {
 
 						strict_required_status_checks_policy = true
 						do_not_enforce_on_create             = true
-
 					}
 
 					required_workflows {
