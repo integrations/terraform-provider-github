63c37ded464c553633551318c4ff607d2ee382e7
Timo Sand <timo.sand@f-secure.com>
2025-12-03 12:49:48 +0200

    Remove unnecessary panic test
    Signed-off-by: Timo Sand <timo.sand@f-secure.com>

--- github/resource_github_organization_ruleset_test.go
+++ github/resource_github_organization_ruleset_test.go
@@ -557,43 +557,6 @@ func TestGithubOrganizationRulesets(t *testing.T) {
 		})
 	})
 
-	t.Run("panics when conditions block is missing for branch target", func(t *testing.T) {
-		config := fmt.Sprintf(`
-			resource "github_organization_ruleset" "test" {
-				name        = "test-no-conditions-%s"
-				target      = "branch"
-				enforcement = "active"
-
-				rules {
-					creation = true
-				}
-			}
-		`, randomID)
-
-		testCase := func(t *testing.T, mode string) {
-			resource.Test(t, resource.TestCase{
-				PreCheck:  func() { skipUnlessMode(t, mode) },
-				Providers: testAccProviders,
-				Steps: []resource.TestStep{
-					{
-						Config: config,
-						// Before fix: This will PANIC with "index out of range"
-						// After fix: Should return proper validation error
-						ExpectError: regexp.MustCompile(`conditions block is required for branch target`),
-					},
-				},
-			})
-		}
-
-		t.Run("with an enterprise account", func(t *testing.T) {
-			testCase(t, enterprise)
-		})
-
-		t.Run("with an organization account", func(t *testing.T) {
-			t.Skip("organization account not supported for this operation, since it needs a paid Team plan.")
-		})
-	})
-
 	t.Run("Validates branch target requires ref_name", func(t *testing.T) {
 		config := fmt.Sprintf(`
 			resource "github_organization_ruleset" "test" {
