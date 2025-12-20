9fc330cd1b5dbb6301870e51a0ee020448c44a91
Pete Steyert-Woods <email@pete-woods.com>
2025-12-20 18:35:57 +0000

    RateLimitRoundtripper: Fix mutex leak and not respecting context cancellation (#2298)
    - Make sure that all exit paths from functions correctly release the mutex.
    - Add a sleep function that respects context cancellation, and use it in-place of time.Sleep.

--- github/transport.go
+++ github/transport.go
@@ -2,6 +2,7 @@ package github
 
 import (
 	"bytes"
+	"context"
 	"errors"
 	"io"
 	"log"
@@ -66,7 +67,7 @@ func (rlt *RateLimitTransport) RoundTrip(req *http.Request) (*http.Response, err
 	// for read and write requests. See isWriteMethod for the distinction between them.
 	if rlt.nextRequestDelay > 0 {
 		log.Printf("[DEBUG] Sleeping %s between operations", rlt.nextRequestDelay)
-		time.Sleep(rlt.nextRequestDelay)
+		sleep(req.Context(), rlt.nextRequestDelay)
 	}
 
 	rlt.nextRequestDelay = rlt.calculateNextDelay(req.Method)
@@ -82,6 +83,7 @@ func (rlt *RateLimitTransport) RoundTrip(req *http.Request) (*http.Response, err
 	// See https://github.com/google/go-github/pull/986
 	r1, r2, err := drainBody(resp.Body)
 	if err != nil {
+		rlt.smartLock(false)
 		return nil, err
 	}
 	resp.Body = r1
@@ -95,7 +97,7 @@ func (rlt *RateLimitTransport) RoundTrip(req *http.Request) (*http.Response, err
 		retryAfter := arlErr.GetRetryAfter()
 		log.Printf("[WARN] Abuse detection mechanism triggered, sleeping for %s before retrying",
 			retryAfter)
-		time.Sleep(retryAfter)
+		sleep(req.Context(), retryAfter)
 		rlt.smartLock(false)
 		return rlt.RoundTrip(req)
 	}
@@ -106,7 +108,7 @@ func (rlt *RateLimitTransport) RoundTrip(req *http.Request) (*http.Response, err
 		retryAfter := time.Until(rlErr.Rate.Reset.Time)
 		log.Printf("[WARN] Rate limit %d reached, sleeping for %s (until %s) before retrying",
 			rlErr.Rate.Limit, retryAfter, time.Now().Add(retryAfter))
-		time.Sleep(retryAfter)
+		sleep(req.Context(), retryAfter)
 		rlt.smartLock(false)
 		return rlt.RoundTrip(req)
 	}
@@ -114,6 +116,17 @@ func (rlt *RateLimitTransport) RoundTrip(req *http.Request) (*http.Response, err
 	rlt.smartLock(false)
 
 	return resp, nil
+}
+
+// sleep is used an alternative to time.Sleep that supports cancellation via the passed context.Context.
+func sleep(ctx context.Context, dur time.Duration) {
+	t := time.NewTimer(dur)
+	defer t.Stop()
+
+	select {
+	case <-t.C:
+	case <-ctx.Done():
+	}
 }
 
 // smartLock wraps the mutex locking system and performs its operation via a boolean input for locking and unlocking.
