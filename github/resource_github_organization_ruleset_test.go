f27b9fa2cd4fc2557f8d421245f6bdeab52fb13c
Timo Sand <timo.sand@f-secure.com>
2025-12-28 13:13:03 +0200

    Migrate to use `terraform-plugin-testing` instead of the SDK testing package
    Signed-off-by: Timo Sand <timo.sand@f-secure.com>

--- github/resource_github_organization_ruleset_test.go
+++ github/resource_github_organization_ruleset_test.go
@@ -5,9 +5,9 @@ import (
 	"regexp"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
 func TestAccGithubOrganizationRuleset(t *testing.T) {
