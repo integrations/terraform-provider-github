0cc6c5b9709adea01c685dd52b94094bf0bd1e51
Timo Sand <timo.sand@f-secure.com>
2025-12-28 13:13:03 +0200

    Migrate to use `terraform-plugin-testing` instead of the SDK testing package
    Signed-off-by: Timo Sand <timo.sand@f-secure.com>

--- github/data_source_github_repository_test.go
+++ github/data_source_github_repository_test.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
 func TestAccDataSourceGithubRepository(t *testing.T) {
