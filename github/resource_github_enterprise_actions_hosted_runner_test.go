f753072df575386d6e5e70779aec8a2d79abb90c
Nicolas Fidel <nicolas.fidel@epita.fr>
2026-01-10 23:17:16 +0100

    fix: review for github_enterprise_actions_hosted_runner

--- github/resource_github_enterprise_actions_hosted_runner_test.go
+++ github/resource_github_enterprise_actions_hosted_runner_test.go
@@ -11,6 +11,9 @@ import (
 func TestAccGithubEnterpriseActionsHostedRunner(t *testing.T) {
 	randomID := acctest.RandStringFromCharSet(5, acctest.CharSetAlphaNum)
 
+	// Image ID "2306" is the GitHub-owned Ubuntu Latest 24.04 image
+	// This is a stable image ID used for acceptance testing
+	// To list available images: GET /enterprises/{enterprise}/actions/hosted-runners/images/github-owned
 	t.Run("creates enterprise hosted runners without error", func(t *testing.T) {
 		config := fmt.Sprintf(`
 			data "github_enterprise" "enterprise" {
@@ -26,9 +29,9 @@ func TestAccGithubEnterpriseActionsHostedRunner(t *testing.T) {
 			resource "github_enterprise_actions_hosted_runner" "test" {
 				enterprise_slug = data.github_enterprise.enterprise.slug
 				name            = "tf-acc-test-%s"
-				
+
 				image {
-					id     = "2306"
+					id     = "2306"  # GitHub-owned Ubuntu Latest 24.04
 					source = "github"
 				}
 
@@ -37,80 +40,29 @@ func TestAccGithubEnterpriseActionsHostedRunner(t *testing.T) {
 			}
 		`, testAccConf.enterpriseSlug, randomID, randomID)
 
-		check := resource.ComposeTestCheckFunc(
-			resource.TestCheckResourceAttr(
-				"github_enterprise_actions_hosted_runner.test", "enterprise_slug",
-				testAccConf.enterpriseSlug,
-			),
-			resource.TestCheckResourceAttr(
-				"github_enterprise_actions_hosted_runner.test", "name",
-				fmt.Sprintf("tf-acc-test-%s", randomID),
-			),
-			resource.TestCheckResourceAttr(
-				"github_enterprise_actions_hosted_runner.test", "size",
-				"4-core",
-			),
-			resource.TestCheckResourceAttr(
-				"github_enterprise_actions_hosted_runner.test", "image.0.id",
-				"2306",
-			),
-			resource.TestCheckResourceAttr(
-				"github_enterprise_actions_hosted_runner.test", "image.0.source",
-				"github",
-			),
-			resource.TestCheckResourceAttrSet(
-				"github_enterprise_actions_hosted_runner.test", "id",
-			),
-			resource.TestCheckResourceAttrSet(
-				"github_enterprise_actions_hosted_runner.test", "status",
-			),
-			resource.TestCheckResourceAttrSet(
-				"github_enterprise_actions_hosted_runner.test", "platform",
-			),
-			resource.TestCheckResourceAttrSet(
-				"github_enterprise_actions_hosted_runner.test", "image.0.size_gb",
-			),
-			resource.TestCheckResourceAttrSet(
-				"github_enterprise_actions_hosted_runner.test", "machine_size_details.0.id",
-			),
-			resource.TestCheckResourceAttrSet(
-				"github_enterprise_actions_hosted_runner.test", "machine_size_details.0.cpu_cores",
-			),
-			resource.TestCheckResourceAttrSet(
-				"github_enterprise_actions_hosted_runner.test", "machine_size_details.0.memory_gb",
-			),
-			resource.TestCheckResourceAttrSet(
-				"github_enterprise_actions_hosted_runner.test", "machine_size_details.0.storage_gb",
-			),
-		)
-
-		testCase := func(t *testing.T, mode testMode) {
-			resource.Test(t, resource.TestCase{
-				PreCheck:  func() { skipUnlessMode(t, mode) },
-				Providers: testAccProviders,
-				Steps: []resource.TestStep{
-					{
-						Config: config,
-						Check:  check,
-					},
+		resource.Test(t, resource.TestCase{
+			PreCheck:          func() { skipUnlessMode(t, enterprise) },
+			ProviderFactories: providerFactories,
+			Steps: []resource.TestStep{
+				{
+					Config: config,
+					Check: resource.ComposeTestCheckFunc(
+						resource.TestCheckResourceAttr("github_enterprise_actions_hosted_runner.test", "enterprise_slug", testAccConf.enterpriseSlug),
+						resource.TestCheckResourceAttr("github_enterprise_actions_hosted_runner.test", "name", fmt.Sprintf("tf-acc-test-%s", randomID)),
+						resource.TestCheckResourceAttr("github_enterprise_actions_hosted_runner.test", "size", "4-core"),
+						resource.TestCheckResourceAttr("github_enterprise_actions_hosted_runner.test", "image.0.id", "2306"),
+						resource.TestCheckResourceAttr("github_enterprise_actions_hosted_runner.test", "image.0.source", "github"),
+						resource.TestCheckResourceAttrSet("github_enterprise_actions_hosted_runner.test", "id"),
+						resource.TestCheckResourceAttrSet("github_enterprise_actions_hosted_runner.test", "status"),
+						resource.TestCheckResourceAttrSet("github_enterprise_actions_hosted_runner.test", "platform"),
+						resource.TestCheckResourceAttrSet("github_enterprise_actions_hosted_runner.test", "image.0.size_gb"),
+						resource.TestCheckResourceAttrSet("github_enterprise_actions_hosted_runner.test", "machine_size_details.0.id"),
+						resource.TestCheckResourceAttrSet("github_enterprise_actions_hosted_runner.test", "machine_size_details.0.cpu_cores"),
+						resource.TestCheckResourceAttrSet("github_enterprise_actions_hosted_runner.test", "machine_size_details.0.memory_gb"),
+						resource.TestCheckResourceAttrSet("github_enterprise_actions_hosted_runner.test", "machine_size_details.0.storage_gb"),
+					),
 				},
-			})
-		}
-
-		t.Run("with an anonymous account", func(t *testing.T) {
-			t.Skip("anonymous account not supported for this operation")
-		})
-
-		t.Run("with an individual account", func(t *testing.T) {
-			t.Skip("individual account not supported for enterprise hosted runners")
-		})
-
-		t.Run("with an organization account", func(t *testing.T) {
-			t.Skip("organization account not supported for enterprise operations")
-		})
-
-		t.Run("with an enterprise account", func(t *testing.T) {
-			testCase(t, enterprise)
+			},
 		})
 	})
 
@@ -169,75 +121,29 @@ func TestAccGithubEnterpriseActionsHostedRunner(t *testing.T) {
 			}
 		`, testAccConf.enterpriseSlug, randomID, randomID)
 
-		check := resource.ComposeTestCheckFunc(
-			resource.TestCheckResourceAttr(
-				"github_enterprise_actions_hosted_runner.test", "name",
-				fmt.Sprintf("tf-acc-test-%s", randomID),
-			),
-			resource.TestCheckResourceAttr(
-				"github_enterprise_actions_hosted_runner.test", "size",
-				"4-core",
-			),
-			resource.TestCheckResourceAttr(
-				"github_enterprise_actions_hosted_runner.test", "maximum_runners",
-				"5",
-			),
-			resource.TestCheckResourceAttr(
-				"github_enterprise_actions_hosted_runner.test", "public_ip_enabled",
-				"false",
-			),
-		)
-
-		checkUpdated := resource.ComposeTestCheckFunc(
-			resource.TestCheckResourceAttr(
-				"github_enterprise_actions_hosted_runner.test", "name",
-				fmt.Sprintf("tf-acc-test-updated-%s", randomID),
-			),
-			resource.TestCheckResourceAttr(
-				"github_enterprise_actions_hosted_runner.test", "size",
-				"8-core",
-			),
-			resource.TestCheckResourceAttr(
-				"github_enterprise_actions_hosted_runner.test", "maximum_runners",
-				"10",
-			),
-			resource.TestCheckResourceAttr(
-				"github_enterprise_actions_hosted_runner.test", "public_ip_enabled",
-				"true",
-			),
-		)
-
-		testCase := func(t *testing.T, mode testMode) {
-			resource.Test(t, resource.TestCase{
-				PreCheck:  func() { skipUnlessMode(t, mode) },
-				Providers: testAccProviders,
-				Steps: []resource.TestStep{
-					{
-						Config: config,
-						Check:  check,
-					},
-					{
-						Config: configUpdated,
-						Check:  checkUpdated,
-					},
+		resource.Test(t, resource.TestCase{
+			PreCheck:          func() { skipUnlessMode(t, enterprise) },
+			ProviderFactories: providerFactories,
+			Steps: []resource.TestStep{
+				{
+					Config: config,
+					Check: resource.ComposeTestCheckFunc(
+						resource.TestCheckResourceAttr("github_enterprise_actions_hosted_runner.test", "name", fmt.Sprintf("tf-acc-test-%s", randomID)),
+						resource.TestCheckResourceAttr("github_enterprise_actions_hosted_runner.test", "size", "4-core"),
+						resource.TestCheckResourceAttr("github_enterprise_actions_hosted_runner.test", "maximum_runners", "5"),
+						resource.TestCheckResourceAttr("github_enterprise_actions_hosted_runner.test", "public_ip_enabled", "false"),
+					),
 				},
-			})
-		}
-
-		t.Run("with an anonymous account", func(t *testing.T) {
-			t.Skip("anonymous account not supported for this operation")
-		})
-
-		t.Run("with an individual account", func(t *testing.T) {
-			t.Skip("individual account not supported for enterprise hosted runners")
-		})
-
-		t.Run("with an organization account", func(t *testing.T) {
-			t.Skip("organization account not supported for enterprise operations")
-		})
-
-		t.Run("with an enterprise account", func(t *testing.T) {
-			testCase(t, enterprise)
+				{
+					Config: configUpdated,
+					Check: resource.ComposeTestCheckFunc(
+						resource.TestCheckResourceAttr("github_enterprise_actions_hosted_runner.test", "name", fmt.Sprintf("tf-acc-test-updated-%s", randomID)),
+						resource.TestCheckResourceAttr("github_enterprise_actions_hosted_runner.test", "size", "8-core"),
+						resource.TestCheckResourceAttr("github_enterprise_actions_hosted_runner.test", "maximum_runners", "10"),
+						resource.TestCheckResourceAttr("github_enterprise_actions_hosted_runner.test", "public_ip_enabled", "true"),
+					),
+				},
+			},
 		})
 	})
 
@@ -267,53 +173,24 @@ func TestAccGithubEnterpriseActionsHostedRunner(t *testing.T) {
 			}
 		`, testAccConf.enterpriseSlug, randomID, randomID)
 
-		check := resource.ComposeTestCheckFunc(
-			resource.TestCheckResourceAttr(
-				"github_enterprise_actions_hosted_runner.test", "enterprise_slug",
-				testAccConf.enterpriseSlug,
-			),
-			resource.TestCheckResourceAttr(
-				"github_enterprise_actions_hosted_runner.test", "name",
-				fmt.Sprintf("tf-acc-test-%s", randomID),
-			),
-			resource.TestCheckResourceAttr(
-				"github_enterprise_actions_hosted_runner.test", "size",
-				"4-core",
-			),
-		)
-
-		testCase := func(t *testing.T, mode testMode) {
-			resource.Test(t, resource.TestCase{
-				PreCheck:  func() { skipUnlessMode(t, mode) },
-				Providers: testAccProviders,
-				Steps: []resource.TestStep{
-					{
-						Config: config,
-						Check:  check,
-					},
-					{
-						ResourceName:      "github_enterprise_actions_hosted_runner.test",
-						ImportState:       true,
-						ImportStateVerify: true,
-					},
+		resource.Test(t, resource.TestCase{
+			PreCheck:          func() { skipUnlessMode(t, enterprise) },
+			ProviderFactories: providerFactories,
+			Steps: []resource.TestStep{
+				{
+					Config: config,
+					Check: resource.ComposeTestCheckFunc(
+						resource.TestCheckResourceAttr("github_enterprise_actions_hosted_runner.test", "enterprise_slug", testAccConf.enterpriseSlug),
+						resource.TestCheckResourceAttr("github_enterprise_actions_hosted_runner.test", "name", fmt.Sprintf("tf-acc-test-%s", randomID)),
+						resource.TestCheckResourceAttr("github_enterprise_actions_hosted_runner.test", "size", "4-core"),
+					),
 				},
-			})
-		}
-
-		t.Run("with an anonymous account", func(t *testing.T) {
-			t.Skip("anonymous account not supported for this operation")
-		})
-
-		t.Run("with an individual account", func(t *testing.T) {
-			t.Skip("individual account not supported for enterprise hosted runners")
-		})
-
-		t.Run("with an organization account", func(t *testing.T) {
-			t.Skip("organization account not supported for enterprise operations")
-		})
-
-		t.Run("with an enterprise account", func(t *testing.T) {
-			testCase(t, enterprise)
+				{
+					ResourceName:      "github_enterprise_actions_hosted_runner.test",
+					ImportState:       true,
+					ImportStateVerify: true,
+				},
+			},
 		})
 	})
 }
