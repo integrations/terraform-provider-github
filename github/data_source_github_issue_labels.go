c4f102a52d5b8904abb06ec753ae637f93b54661
Timo Sand <timo.sand@f-secure.com>
2026-01-25 22:10:57 +0200

    fix(lint): add schema descriptions to github_issue_labels data source

--- github/data_source_github_issue_labels.go
+++ github/data_source_github_issue_labels.go
@@ -12,29 +12,35 @@ func dataSourceGithubIssueLabels() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"repository": {
-				Type:     schema.TypeString,
-				Required: true,
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The name of the repository.",
 			},
 			"labels": {
-				Type:     schema.TypeList,
-				Computed: true,
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: "The list of this repository's labels.",
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"name": {
-							Type:     schema.TypeString,
-							Computed: true,
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The name of the label.",
 						},
 						"color": {
-							Type:     schema.TypeString,
-							Computed: true,
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The hexadecimal color code for the label, without the leading #.",
 						},
 						"description": {
-							Type:     schema.TypeString,
-							Computed: true,
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "A short description of the label.",
 						},
 						"url": {
-							Type:     schema.TypeString,
-							Computed: true,
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The URL of the label.",
 						},
 					},
 				},
