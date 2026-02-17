3795914047b43dd78fb888579e56768846ca9925
Victor M. Varela <vmvarela@gmail.com>
2026-02-17 14:48:25 +0100

    test(enterprise-teams): migrate tests to terraform-plugin-testing and fix import verification

--- github/resource_github_enterprise_team_test.go
+++ github/resource_github_enterprise_team_test.go
@@ -6,8 +6,8 @@ import (
 	"regexp"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
 func TestAccGithubEnterpriseTeam(t *testing.T) {
@@ -81,10 +81,11 @@ func TestAccGithubEnterpriseTeam(t *testing.T) {
 					`, testAccConf.enterpriseSlug, testResourcePrefix, randomID),
 				},
 				{
-					ResourceName:        "github_enterprise_team.test",
-					ImportState:         true,
-					ImportStateVerify:   true,
-					ImportStateIdPrefix: fmt.Sprintf(`%s/`, testAccConf.enterpriseSlug),
+					ResourceName:            "github_enterprise_team.test",
+					ImportState:             true,
+					ImportStateVerify:       true,
+					ImportStateVerifyIgnore: []string{"group_id"},
+					ImportStateIdPrefix:     fmt.Sprintf(`%s/`, testAccConf.enterpriseSlug),
 				},
 			},
 		})
