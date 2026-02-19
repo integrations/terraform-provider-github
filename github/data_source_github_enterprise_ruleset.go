08cc9aa9e7f422f88ced98c12ebf858059c36c89
luka-kroeger <luka.kroeger@siemens.com>
2026-02-19 08:30:35 +0100

    fix(enterprise_ruleset): add description to GitHub enterprise ruleset data source and improve test checks

--- github/data_source_github_enterprise_ruleset.go
+++ github/data_source_github_enterprise_ruleset.go
@@ -14,6 +14,7 @@ import (
 
 func dataSourceGithubEnterpriseRuleset() *schema.Resource {
 	return &schema.Resource{
+		Description: "Use this data source to retrieve information about a GitHub enterprise ruleset.",
 		ReadContext: dataSourceGithubEnterpriseRulesetRead,
 
 		Schema: map[string]*schema.Schema{
