84f402ab0db5614b3049cac544be2f041dc110bb
Timo Sand <timo.sand@f-secure.com>
2026-03-08 18:34:27 +0200

    Add `L002` check to ensure all new resources use Context-aware functions
    Signed-off-by: Timo Sand <timo.sand@f-secure.com>

--- tools/tfproviderlint/checks/checks.go
+++ tools/tfproviderlint/checks/checks.go
@@ -3,9 +3,11 @@ package checks
 import (
 	"golang.org/x/tools/go/analysis"
 
-	L001 "github.com/integrations/terraform-provider-github/tools/tfproviderlint/checks/L001"
+	"github.com/integrations/terraform-provider-github/tools/tfproviderlint/checks/L001"
+	"github.com/integrations/terraform-provider-github/tools/tfproviderlint/checks/L002"
 )
 
 var AllChecks = []*analysis.Analyzer{
 	L001.Analyzer,
+	L002.Analyzer,
 }
