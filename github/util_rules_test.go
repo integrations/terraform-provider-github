e31de46044d63f989bd6ae879c9d7401e18a9681
Steve Hipwell <steve.hipwell@gmail.com>
2026-03-16 22:25:54 +0000

    chore: Update go-github to v84 (#3238)
    Signed-off-by: Steve Hipwell <steve.hipwell@gmail.com>

--- github/util_rules_test.go
+++ github/util_rules_test.go
@@ -3,7 +3,7 @@ package github
 import (
 	"testing"
 
-	"github.com/google/go-github/v83/github"
+	"github.com/google/go-github/v84/github"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
