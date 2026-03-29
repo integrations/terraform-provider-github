92a7024004eede53bec0e2bb83ddea9380670895
Timo Sand <timo.sand@f-secure.com>
2026-03-29 21:08:54 +0300

    Add validation for merge and squash merge configs
    Signed-off-by: Timo Sand <timo.sand@f-secure.com>

--- github/resource_github_repository.go
+++ github/resource_github_repository.go
@@ -258,28 +258,32 @@ func resourceGithubRepository() *schema.Resource {
 				Description: "Configure private forking for organization owned private and internal repositories; set to 'true' to enable, 'false' to disable, and leave unset for the default behaviour. Configuring this requires that private forking is not being explicitly configured at the organization level.",
 			},
 			"squash_merge_commit_title": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     "COMMIT_OR_PR_TITLE",
-				Description: "Can be 'PR_TITLE' or 'COMMIT_OR_PR_TITLE' for a default squash merge commit title. Applicable only if 'allow_squash_merge' is 'true'.",
+				Type:         schema.TypeString,
+				Optional:     true,
+				Default:      "COMMIT_OR_PR_TITLE",
+				Description:  "Can be 'PR_TITLE' or 'COMMIT_OR_PR_TITLE' for a default squash merge commit title. Applicable only if 'allow_squash_merge' is 'true'.",
+				RequiredWith: []string{"allow_squash_merge"},
 			},
 			"squash_merge_commit_message": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     "COMMIT_MESSAGES",
-				Description: "Can be 'PR_BODY', 'COMMIT_MESSAGES', or 'BLANK' for a default squash merge commit message. Applicable only if 'allow_squash_merge' is 'true'.",
+				Type:         schema.TypeString,
+				Optional:     true,
+				Default:      "COMMIT_MESSAGES",
+				Description:  "Can be 'PR_BODY', 'COMMIT_MESSAGES', or 'BLANK' for a default squash merge commit message. Applicable only if 'allow_squash_merge' is 'true'.",
+				RequiredWith: []string{"allow_squash_merge", "squash_merge_commit_title"},
 			},
 			"merge_commit_title": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     "MERGE_MESSAGE",
-				Description: "Can be 'PR_TITLE' or 'MERGE_MESSAGE' for a default merge commit title. Applicable only if 'allow_merge_commit' is 'true'.",
+				Type:         schema.TypeString,
+				Optional:     true,
+				Default:      "MERGE_MESSAGE",
+				Description:  "Can be 'PR_TITLE' or 'MERGE_MESSAGE' for a default merge commit title. Applicable only if 'allow_merge_commit' is 'true'.",
+				RequiredWith: []string{"allow_merge_commit"},
 			},
 			"merge_commit_message": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     "PR_TITLE",
-				Description: "Can be 'PR_BODY', 'PR_TITLE', or 'BLANK' for a default merge commit message. Applicable only if 'allow_merge_commit' is 'true'.",
+				Type:         schema.TypeString,
+				Optional:     true,
+				Default:      "PR_TITLE",
+				Description:  "Can be 'PR_BODY', 'PR_TITLE', or 'BLANK' for a default merge commit message. Applicable only if 'allow_merge_commit' is 'true'.",
+				RequiredWith: []string{"allow_merge_commit", "merge_commit_title"},
 			},
 			"delete_branch_on_merge": {
 				Type:        schema.TypeBool,
@@ -524,6 +528,21 @@ func customDiffFunction(_ context.Context, diff *schema.ResourceDiff, v any) err
 			return err
 		}
 	}
+	_, titleOk := diff.GetOk("squash_merge_commit_title")
+	_, messageOk := diff.GetOk("squash_merge_commit_message")
+	if messageOk && titleOk {
+		if !diff.Get("allow_squash_merge").(bool) {
+			return fmt.Errorf("allow_squash_merge is required when squash_merge_commit_title and squash_merge_commit_message is set")
+		}
+	}
+
+	_, titleOk = diff.GetOk("merge_commit_title")
+	_, messageOk = diff.GetOk("merge_commit_message")
+	if messageOk && titleOk {
+		if !diff.Get("allow_merge_commit").(bool) {
+			return fmt.Errorf("allow_merge_commit is required when merge_commit_title and merge_commit_message is set")
+		}
+	}
 	return nil
 }
 
@@ -757,7 +776,7 @@ func resourceGithubRepositoryCreate(ctx context.Context, d *schema.ResourceData,
 		if meta.IsOrganization {
 			repoOwner = owner
 		} else {
-			// TODO: This is deprecated since `owner` should already be set to the authenticated user
+			// Note: This exists due to the `go-github` library, which chooses endpoints based on the owner being empty or not
 			tflog.Info(ctx, "Creating repository for authenticated user")
 			repoOwner = ""
 		}
