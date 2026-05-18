81514ae12285149e0340c6d6aaf94796c9ff636c
Niklas Beinghaus <niklas.beinghaus@fiskaly.com>
2026-05-18 16:37:06 +0200

    [FEAT] Support looking up github_membership data source by user_id
    Add an optional 'user_id' input (mutually exclusive with 'username') to the
    github_membership data source. When set, the user is resolved via
    GET /user/{id} and the resulting login is used to query the org
    membership endpoint, which only accepts logins.
    
    The data source now always exposes 'user_id' as a computed attribute too,
    so downstream resources can refer to the stable numeric ID even when the
    query used a login.
    
    Refs #524

--- github/data_source_github_membership_test.go
+++ github/data_source_github_membership_test.go
@@ -26,6 +26,7 @@ func TestAccGithubMembershipDataSource(t *testing.T) {
 			resource.TestCheckResourceAttrSet("data.github_membership.test", "role"),
 			resource.TestCheckResourceAttrSet("data.github_membership.test", "etag"),
 			resource.TestCheckResourceAttrSet("data.github_membership.test", "state"),
+			resource.TestCheckResourceAttrSet("data.github_membership.test", "user_id"),
 		)
 
 		resource.Test(t, resource.TestCase{
@@ -55,6 +56,106 @@ func TestAccGithubMembershipDataSource(t *testing.T) {
 				{
 					Config:      config,
 					ExpectError: regexp.MustCompile(`Not Found`),
+				},
+			},
+		})
+	})
+
+	t.Run("queries the membership for a user by user_id", func(t *testing.T) {
+		ctx := t.Context()
+
+		meta, err := getTestMeta()
+		if err != nil {
+			t.Fatalf("failed to get test meta: %s", err)
+		}
+
+		ghUser, _, err := meta.v3client.Users.Get(ctx, testAccConf.testOrgUser)
+		if err != nil {
+			t.Fatalf("failed to resolve org user id: %s", err)
+		}
+
+		config := fmt.Sprintf(`
+			data "github_membership" "test" {
+				user_id      = %d
+				organization = "%s"
+			}
+		`, ghUser.GetID(), testAccConf.owner)
+
+		check := resource.ComposeTestCheckFunc(
+			resource.TestCheckResourceAttr("data.github_membership.test", "username", testAccConf.testOrgUser),
+			resource.TestCheckResourceAttr("data.github_membership.test", "user_id", fmt.Sprintf("%d", ghUser.GetID())),
+			resource.TestCheckResourceAttrSet("data.github_membership.test", "role"),
+			resource.TestCheckResourceAttrSet("data.github_membership.test", "etag"),
+			resource.TestCheckResourceAttrSet("data.github_membership.test", "state"),
+		)
+
+		resource.Test(t, resource.TestCase{
+			PreCheck:          func() { skipUnlessHasOrgs(t) },
+			ProviderFactories: providerFactories,
+			Steps: []resource.TestStep{
+				{
+					Config: config,
+					Check:  check,
+				},
+			},
+		})
+	})
+
+	t.Run("errors when querying with non-existent user_id", func(t *testing.T) {
+		config := fmt.Sprintf(`
+			data "github_membership" "test" {
+				user_id      = 999999999999
+				organization = "%s"
+			}
+		`, testAccConf.owner)
+
+		resource.Test(t, resource.TestCase{
+			PreCheck:          func() { skipUnlessHasOrgs(t) },
+			ProviderFactories: providerFactories,
+			Steps: []resource.TestStep{
+				{
+					Config:      config,
+					ExpectError: regexp.MustCompile(`Not Found`),
+				},
+			},
+		})
+	})
+
+	t.Run("errors when neither username nor user_id is provided", func(t *testing.T) {
+		config := fmt.Sprintf(`
+			data "github_membership" "test" {
+				organization = "%s"
+			}
+		`, testAccConf.owner)
+
+		resource.Test(t, resource.TestCase{
+			PreCheck:          func() { skipUnlessHasOrgs(t) },
+			ProviderFactories: providerFactories,
+			Steps: []resource.TestStep{
+				{
+					Config:      config,
+					ExpectError: regexp.MustCompile(`one of (\x60username\x60,\x60user_id\x60|\x60user_id\x60,\x60username\x60) must be specified`),
+				},
+			},
+		})
+	})
+
+	t.Run("errors when both username and user_id are provided", func(t *testing.T) {
+		config := fmt.Sprintf(`
+			data "github_membership" "test" {
+				username     = "%s"
+				user_id      = 1
+				organization = "%s"
+			}
+		`, testAccConf.testOrgUser, testAccConf.owner)
+
+		resource.Test(t, resource.TestCase{
+			PreCheck:          func() { skipUnlessHasOrgs(t) },
+			ProviderFactories: providerFactories,
+			Steps: []resource.TestStep{
+				{
+					Config:      config,
+					ExpectError: regexp.MustCompile(`only one of (\x60user_id\x60,\x60username\x60|\x60username\x60,\x60user_id\x60) can be specified`),
 				},
 			},
 		})
