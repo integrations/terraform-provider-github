64b32a5487618395d6e2649d0872cc546c0a4e34
Lasse Gaardsholt <lasse.gaardsholt@bestseller.com>
2026-06-04 22:49:44 +0200

    update go-github  to v88 to align with the rest of the provider
    Signed-off-by: Lasse Gaardsholt <lasse.gaardsholt@bestseller.com>

--- github/resource_github_organization_private_registry.go
+++ github/resource_github_organization_private_registry.go
@@ -5,7 +5,7 @@ import (
 	"encoding/base64"
 	"errors"
 
-	"github.com/google/go-github/v86/github"
+	"github.com/google/go-github/v88/github"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
