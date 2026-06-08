09bd2d940d9be27b25a69e91e37fe47f91182ddb
Lasse Gaardsholt <lasse.gaardsholt@bestseller.com>
2026-06-08 14:43:32 +0200

    Added test for import
    Signed-off-by: Lasse Gaardsholt <lasse.gaardsholt@bestseller.com>

--- github/resource_github_organization_private_registry_test.go
+++ github/resource_github_organization_private_registry_test.go
@@ -23,12 +23,34 @@ func TestAccGithubOrganizationPrivateRegistry_basic(t *testing.T) {
 		}
 	`
 
+	updatedConfig := `
+		resource "github_organization_private_registry" "test" {
+			registry_type  = "npm_registry"
+			url            = "https://npm-registry.example.com"
+			username       = "github-actions"
+			secret         = "super_secret_token_123"
+			visibility     = "all"
+		}
+
+		data "github_organization_private_registry" "test" {
+			name = github_organization_private_registry.test.name
+		}
+	`
+
 	check := resource.ComposeTestCheckFunc(
 		resource.TestCheckResourceAttrSet("github_organization_private_registry.test", "name"),
 		resource.TestCheckResourceAttr("github_organization_private_registry.test", "registry_type", "npm_registry"),
 		resource.TestCheckResourceAttr("github_organization_private_registry.test", "url", "https://npm.pkg.github.com"),
 		resource.TestCheckResourceAttr("github_organization_private_registry.test", "username", "github-actions"),
 		resource.TestCheckResourceAttr("github_organization_private_registry.test", "visibility", "private"),
+		resource.TestCheckResourceAttr("data.github_organization_private_registry.test", "registry_type", "npm_registry"),
+	)
+	updatedCheck := resource.ComposeTestCheckFunc(
+		resource.TestCheckResourceAttrSet("github_organization_private_registry.test", "name"),
+		resource.TestCheckResourceAttr("github_organization_private_registry.test", "registry_type", "npm_registry"),
+		resource.TestCheckResourceAttr("github_organization_private_registry.test", "url", "https://npm-registry.example.com"),
+		resource.TestCheckResourceAttr("github_organization_private_registry.test", "username", "github-actions"),
+		resource.TestCheckResourceAttr("github_organization_private_registry.test", "visibility", "all"),
 		resource.TestCheckResourceAttr("data.github_organization_private_registry.test", "registry_type", "npm_registry"),
 	)
 
@@ -39,6 +61,16 @@ func TestAccGithubOrganizationPrivateRegistry_basic(t *testing.T) {
 			{
 				Config: config,
 				Check:  check,
+			},
+			{
+				ResourceName:            "github_organization_private_registry.test",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"secret"},
+			},
+			{
+				Config: updatedConfig,
+				Check:  updatedCheck,
 			},
 		},
 	})
