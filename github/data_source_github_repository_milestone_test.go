cab3e68981679c06d46316816fdd772c589735e7
Steve Hipwell <steve.hipwell@gmail.com>
2026-06-25 15:53:50 +0100

    fixup! chore: Enable acceptance test automation

--- github/data_source_github_repository_milestone_test.go
+++ github/data_source_github_repository_milestone_test.go
@@ -4,46 +4,40 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
-
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
 )
 
 func TestAccGithubRepositoryMilestoneDataSource(t *testing.T) {
 	t.Parallel()
 
 	t.Run("queries a repository milestone", func(t *testing.T) {
-		randomID := acctest.RandStringFromCharSet(5, acctest.CharSetAlphaNum)
-		repoName := fmt.Sprintf("%srepo-milestone-%s", testResourcePrefix, randomID)
+		t.Parallel()
+
+		repo := mustCreateTestRepository(t)
+		title := "v1.0.0"
+		description := "General Availability"
+		dueDate := "2020-11-22"
+		state := "closed"
+
 		config := fmt.Sprintf(`
+resource "github_repository_milestone" "test" {
+  owner       = "%s"
+  repository  = "%s"
+  title       = "%s"
+  description = "%s"
+  due_date    = "%s"
+  state       = "%s"
+}
 
-			resource "github_repository" "test" {
-				name      = "%s"
-			}
-
-			resource "github_repository_milestone" "test" {
-				owner = split("/", "${github_repository.test.full_name}")[0]
-				repository = github_repository.test.name
-		    title = "v1.0.0"
-		    description = "General Availability"
-		    due_date = "2020-11-22"
-		    state = "closed"
-			}
-
-			data "github_repository_milestone" "test" {
-				owner = github_repository_milestone.test.owner
-				repository = github_repository_milestone.test.repository
-		   	number = github_repository_milestone.test.number
-			}
-
-		`, repoName)
-
-		check := resource.ComposeTestCheckFunc(
-			resource.TestCheckResourceAttr(
-				"github_repository_milestone.test", "state",
-				"closed",
-			),
-		)
+data "github_repository_milestone" "test" {
+	owner      = github_repository_milestone.test.owner
+	repository = github_repository_milestone.test.repository
+	number     = github_repository_milestone.test.number
+}
+`, repo.GetOwner().GetLogin(), repo.GetName(), title, description, dueDate, state)
 
 		resource.Test(t, resource.TestCase{
 			PreCheck:          func() { skipUnauthenticated(t) },
@@ -51,7 +45,12 @@ func TestAccGithubRepositoryMilestoneDataSource(t *testing.T) {
 			Steps: []resource.TestStep{
 				{
 					Config: config,
-					Check:  check,
+					ConfigStateChecks: []statecheck.StateCheck{
+						statecheck.ExpectKnownValue("data.github_repository_milestone.test", tfjsonpath.New("title"), knownvalue.StringExact(title)),
+						statecheck.ExpectKnownValue("data.github_repository_milestone.test", tfjsonpath.New("description"), knownvalue.StringExact(description)),
+						statecheck.ExpectKnownValue("data.github_repository_milestone.test", tfjsonpath.New("due_date"), knownvalue.StringExact(dueDate)),
+						statecheck.ExpectKnownValue("data.github_repository_milestone.test", tfjsonpath.New("state"), knownvalue.StringExact(state)),
+					},
 				},
 			},
 		})
--- github/resource_github_actions_organization_permissions_test.go
+++ github/resource_github_actions_organization_permissions_test.go
@@ -74,28 +74,12 @@ func TestAccGithubActionsOrganizationPermissions(t *testing.T) {
 			}
 		`, repoName, allowedActions, enabledRepositories, githubOwnedAllowed, verifiedAllowed, shaPinningRequired)
 
-		check := resource.ComposeTestCheckFunc(
-			resource.TestCheckResourceAttr(
-				"github_actions_organization_permissions.test", "allowed_actions", allowedActions,
-			),
-			resource.TestCheckResourceAttr(
-				"github_actions_organization_permissions.test", "enabled_repositories", enabledRepositories,
-			),
-			resource.TestCheckResourceAttr(
-				"github_actions_organization_permissions.test", "allowed_actions_config.#", "1",
-			),
-			resource.TestCheckResourceAttr(
-				"github_actions_organization_permissions.test", "enabled_repositories_config.#", "1",
-			),
-		)
-
 		resource.Test(t, resource.TestCase{
 			PreCheck:          func() { skipUnlessHasOrgs(t) },
 			ProviderFactories: providerFactories,
 			Steps: []resource.TestStep{
 				{
 					Config: config,
-					Check:  check,
 				},
 				{
 					ResourceName:      "github_actions_organization_permissions.test",
