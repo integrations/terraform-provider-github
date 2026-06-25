826bc1a91f5ceb0f03bd1eba39c7054ebde7ad72
Steve Hipwell <steve.hipwell@gmail.com>
2026-06-25 17:24:00 +0100

    fixup! chore: Enable acceptance test automation

--- github/resource_github_actions_organization_permissions_test.go
+++ github/resource_github_actions_organization_permissions_test.go
@@ -4,222 +4,63 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
 )
 
 func TestAccGithubActionsOrganizationPermissions(t *testing.T) {
 	// IMPORTANT: Do not run these tests in parallel as they modify the organization state.
 
-	t.Run("test setting of basic actions organization permissions", func(t *testing.T) {
-		allowedActions := "local_only"
-		enabledRepositories := "all"
+	t.Run("full_lifecycle", func(t *testing.T) {
+		repo := mustCreateTestRepository(t)
 
-		config := fmt.Sprintf(`
-			resource "github_actions_organization_permissions" "test" {
-				allowed_actions = "%s"
-				enabled_repositories = "%s"
-			}
-		`, allowedActions, enabledRepositories)
+		configMinimal := `
+resource "github_actions_organization_permissions" "test" {
+  allowed_actions      = "all"
+  enabled_repositories = "all"
+}
+`
 
-		check := resource.ComposeTestCheckFunc(
-			resource.TestCheckResourceAttr(
-				"github_actions_organization_permissions.test", "allowed_actions", allowedActions,
-			),
-			resource.TestCheckResourceAttr(
-				"github_actions_organization_permissions.test", "enabled_repositories", enabledRepositories,
-			),
-		)
+		configFull := fmt.Sprintf(`
+resource "github_actions_organization_permissions" "test" {
+  allowed_actions      = "selected"
+  enabled_repositories = "selected"
+
+  allowed_actions_config {
+    github_owned_allowed = true
+    patterns_allowed     = ["actions/cache@*", "actions/checkout@*"]
+    verified_allowed     = true
+  }
+
+  enabled_repositories_config {
+    repository_ids = [%d]
+  }
+
+  sha_pinning_required = true
+}
+`, repo.GetID())
 
 		resource.Test(t, resource.TestCase{
 			PreCheck:          func() { skipUnlessHasOrgs(t) },
 			ProviderFactories: providerFactories,
 			Steps: []resource.TestStep{
 				{
-					Config: config,
-					Check:  check,
+					Config: configMinimal,
+					ConfigStateChecks: []statecheck.StateCheck{
+						statecheck.ExpectKnownValue("github_actions_organization_permissions.test", tfjsonpath.New("allowed_actions_config"), knownvalue.ListSizeExact(0)),
+						statecheck.ExpectKnownValue("github_actions_organization_permissions.test", tfjsonpath.New("enabled_repositories_config"), knownvalue.ListSizeExact(0)),
+						statecheck.ExpectKnownValue("github_actions_organization_permissions.test", tfjsonpath.New("sha_pinning_required"), knownvalue.NotNull()),
+					},
 				},
-			},
-		})
-	})
-
-	t.Run("imports entire set of github action organization permissions without error", func(t *testing.T) {
-		allowedActions := "selected"
-		enabledRepositories := "selected"
-		githubOwnedAllowed := true
-		verifiedAllowed := true
-		shaPinningRequired := true
-		randomID := acctest.RandStringFromCharSet(5, acctest.CharSetAlphaNum)
-		repoName := fmt.Sprintf("%srepo-act-org-perm-%s", testResourcePrefix, randomID)
-
-		config := fmt.Sprintf(`
-			resource "github_repository" "test" {
-				name        = "%[1]s"
-				description = "Terraform acceptance tests %[1]s"
-				topics			= ["terraform", "testing"]
-			}
-
-			resource "github_actions_organization_permissions" "test" {
-				allowed_actions = "%s"
-				enabled_repositories = "%s"
-				allowed_actions_config {
-					github_owned_allowed = %t
-					patterns_allowed     = ["actions/cache@*", "actions/checkout@*"]
-					verified_allowed     = %t
-				}
-				sha_pinning_required = %t
-				enabled_repositories_config {
-					repository_ids       = [github_repository.test.repo_id]
-				}
-			}
-		`, repoName, allowedActions, enabledRepositories, githubOwnedAllowed, verifiedAllowed, shaPinningRequired)
-
-		resource.Test(t, resource.TestCase{
-			PreCheck:          func() { skipUnlessHasOrgs(t) },
-			ProviderFactories: providerFactories,
-			Steps: []resource.TestStep{
 				{
-					Config: config,
+					Config: configFull,
 				},
 				{
 					ResourceName:      "github_actions_organization_permissions.test",
 					ImportState:       true,
 					ImportStateVerify: true,
-				},
-			},
-		})
-	})
-
-	t.Run("test setting of organization allowed actions", func(t *testing.T) {
-		allowedActions := "selected"
-		enabledRepositories := "all"
-		githubOwnedAllowed := true
-		verifiedAllowed := true
-
-		config := fmt.Sprintf(`
-
-			resource "github_actions_organization_permissions" "test" {
-				allowed_actions = "%s"
-				enabled_repositories = "%s"
-				allowed_actions_config {
-					github_owned_allowed = %t
-					patterns_allowed     = ["actions/cache@*", "actions/checkout@*"]
-					verified_allowed     = %t
-				}
-			}
-		`, allowedActions, enabledRepositories, githubOwnedAllowed, verifiedAllowed)
-
-		check := resource.ComposeTestCheckFunc(
-			resource.TestCheckResourceAttr(
-				"github_actions_organization_permissions.test", "allowed_actions", allowedActions,
-			),
-			resource.TestCheckResourceAttr(
-				"github_actions_organization_permissions.test", "enabled_repositories", enabledRepositories,
-			),
-			resource.TestCheckResourceAttr(
-				"github_actions_organization_permissions.test", "allowed_actions_config.#", "1",
-			),
-		)
-
-		resource.Test(t, resource.TestCase{
-			PreCheck:          func() { skipUnlessHasOrgs(t) },
-			ProviderFactories: providerFactories,
-			Steps: []resource.TestStep{
-				{
-					Config: config,
-					Check:  check,
-				},
-			},
-		})
-	})
-
-	t.Run("test not setting of organization allowed actions without error", func(t *testing.T) {
-		allowedActions := "selected"
-		enabledRepositories := "all"
-
-		config := fmt.Sprintf(`
-
-			resource "github_actions_organization_permissions" "test" {
-				allowed_actions = "%s"
-				enabled_repositories = "%s"
-			}
-		`, allowedActions, enabledRepositories)
-
-		check := resource.ComposeTestCheckFunc(
-			resource.TestCheckResourceAttr(
-				"github_actions_organization_permissions.test", "allowed_actions", allowedActions,
-			),
-			resource.TestCheckResourceAttr(
-				"github_actions_organization_permissions.test", "enabled_repositories", enabledRepositories,
-			),
-			resource.TestCheckResourceAttr(
-				"github_actions_organization_permissions.test", "allowed_actions_config.#", "0",
-			),
-		)
-
-		resource.Test(t, resource.TestCase{
-			PreCheck:          func() { skipUnlessHasOrgs(t) },
-			ProviderFactories: providerFactories,
-			Steps: []resource.TestStep{
-				{
-					Config: config,
-					Check:  check,
-				},
-			},
-		})
-	})
-
-	t.Run("test setting of organization enabled repositories", func(t *testing.T) {
-		allowedActions := "all"
-		enabledRepositories := "selected"
-		githubOwnedAllowed := true
-		verifiedAllowed := true
-		randomID := acctest.RandStringFromCharSet(5, acctest.CharSetAlphaNum)
-		randomID2 := acctest.RandStringFromCharSet(5, acctest.CharSetAlphaNum)
-		repoName := fmt.Sprintf("%srepo-act-org-perm-%s", testResourcePrefix, randomID)
-		repoName2 := fmt.Sprintf("%srepo-act-org-perm-%s", testResourcePrefix, randomID2)
-
-		config := fmt.Sprintf(`
-			resource "github_repository" "test" {
-				name        = "%[1]s"
-				description = "Terraform acceptance tests %[1]s"
-				topics			= ["terraform", "testing"]
-			}
-
-			resource "github_repository" "test2" {
-				name        = "%[2]s"
-				description = "Terraform acceptance tests %[2]s"
-				topics			= ["terraform", "testing"]
-			}
-
-			resource "github_actions_organization_permissions" "test" {
-				allowed_actions = "%s"
-				enabled_repositories = "%s"
-				enabled_repositories_config {
-					repository_ids       = [github_repository.test.repo_id, github_repository.test2.repo_id]
-				}
-			}
-		`, repoName, repoName2, allowedActions, enabledRepositories, githubOwnedAllowed, verifiedAllowed)
-
-		check := resource.ComposeTestCheckFunc(
-			resource.TestCheckResourceAttr(
-				"github_actions_organization_permissions.test", "allowed_actions", allowedActions,
-			),
-			resource.TestCheckResourceAttr(
-				"github_actions_organization_permissions.test", "enabled_repositories", enabledRepositories,
-			),
-			resource.TestCheckResourceAttr(
-				"github_actions_organization_permissions.test", "enabled_repositories_config.#", "1",
-			),
-		)
-
-		resource.Test(t, resource.TestCase{
-			PreCheck:          func() { skipUnlessHasOrgs(t) },
-			ProviderFactories: providerFactories,
-			Steps: []resource.TestStep{
-				{
-					Config: config,
-					Check:  check,
 				},
 			},
 		})
--- github/resource_github_organization_custom_properties_test.go
+++ github/resource_github_organization_custom_properties_test.go
@@ -5,6 +5,7 @@ import (
 	"regexp"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
@@ -14,14 +15,17 @@ func TestAccGithubOrganizationCustomProperties(t *testing.T) {
 	t.Run("rejects invalid values_editable_by value", func(t *testing.T) {
 		t.Parallel()
 
-		config := `
-		resource "github_organization_custom_properties" "test" {
-			property_name      = "TestInvalidValuesEditableBy"
-			value_type         = "string"
-			required           = false
-			description        = "Test invalid values_editable_by"
-			values_editable_by = "invalid_value"
-		}`
+		name := fmt.Sprintf("%s%s", testResourcePrefix, acctest.RandString(5))
+
+		config := fmt.Sprintf(`
+resource "github_organization_custom_properties" "test" {
+  property_name      = "%s"
+  value_type         = "string"
+  required           = false
+  description        = "Test invalid values_editable_by"
+  values_editable_by = "invalid_value"
+}
+`, name)
 
 		resource.Test(t, resource.TestCase{
 			PreCheck:          func() { skipUnlessHasOrgs(t) },
@@ -38,22 +42,18 @@ func TestAccGithubOrganizationCustomProperties(t *testing.T) {
 	t.Run("creates custom property without error", func(t *testing.T) {
 		t.Parallel()
 
-		config := `
-		resource "github_organization_custom_properties" "test" {
-			allowed_values = [ "Test" ]
-			description    = "Test Description"
-			default_value  = "Test"
-			property_name  = "Test"
-			required       = true
-			value_type     = "single_select"
-		  }`
+		name := fmt.Sprintf("%s%s", testResourcePrefix, acctest.RandString(5))
 
-		check := resource.ComposeTestCheckFunc(
-			resource.TestCheckResourceAttr(
-				"github_organization_custom_properties.test",
-				"property_name", "Test",
-			),
-		)
+		config := fmt.Sprintf(`
+resource "github_organization_custom_properties" "test" {
+  allowed_values = [ "Test" ]
+  description    = "Test Description"
+  default_value  = "Test"
+  property_name  = "%s"
+  required       = true
+  value_type     = "single_select"
+}
+`, name)
 
 		resource.Test(t, resource.TestCase{
 			PreCheck:          func() { skipUnlessHasOrgs(t) },
@@ -61,7 +61,9 @@ func TestAccGithubOrganizationCustomProperties(t *testing.T) {
 			Steps: []resource.TestStep{
 				{
 					Config: config,
-					Check:  check,
+					Check: resource.ComposeTestCheckFunc(
+						resource.TestCheckResourceAttr("github_organization_custom_properties.test", "property_name", name),
+					),
 				},
 			},
 		})
@@ -70,30 +72,25 @@ func TestAccGithubOrganizationCustomProperties(t *testing.T) {
 	t.Run("create custom property and update them", func(t *testing.T) {
 		t.Parallel()
 
-		configBefore := `
-		resource "github_organization_custom_properties" "test" {
-			allowed_values = ["one"]
-			description    = "Test Description"
-			property_name  = "Test"
-			value_type     = "single_select"
-		}`
+		name := fmt.Sprintf("%s%s", testResourcePrefix, acctest.RandString(5))
 
-		configAfter := `
-		resource "github_organization_custom_properties" "test" {
-			allowed_values = ["one", "two"]
-			description    = "Test Description 2"
-			property_name  = "Test"
-			value_type     = "single_select"
-		}`
+		configBefore := fmt.Sprintf(`
+resource "github_organization_custom_properties" "test" {
+  allowed_values = ["one"]
+  description    = "Test Description"
+  property_name  = "%s"
+  value_type     = "single_select"
+}
+`, name)
 
-		const resourceName = "github_organization_custom_properties.test"
-
-		checkBefore := resource.ComposeTestCheckFunc(
-			resource.TestCheckResourceAttr(resourceName, "allowed_values.#", "1"),
-		)
-		checkAfter := resource.ComposeTestCheckFunc(
-			resource.TestCheckResourceAttr(resourceName, "allowed_values.#", "2"),
-		)
+		configAfter := fmt.Sprintf(`
+resource "github_organization_custom_properties" "test" {
+  allowed_values = ["one", "two"]
+  description    = "Test Description 2"
+  property_name  = "%s"
+  value_type     = "single_select"
+}
+	`, name)
 
 		resource.Test(t, resource.TestCase{
 			PreCheck:          func() { skipUnlessHasOrgs(t) },
@@ -101,11 +98,15 @@ func TestAccGithubOrganizationCustomProperties(t *testing.T) {
 			Steps: []resource.TestStep{
 				{
 					Config: configBefore,
-					Check:  checkBefore,
+					Check: resource.ComposeTestCheckFunc(
+						resource.TestCheckResourceAttr("github_organization_custom_properties.test", "allowed_values.#", "1"),
+					),
 				},
 				{
 					Config: configAfter,
-					Check:  checkAfter,
+					Check: resource.ComposeTestCheckFunc(
+						resource.TestCheckResourceAttr("github_organization_custom_properties.test", "allowed_values.#", "2"),
+					),
 				},
 			},
 		})
@@ -115,7 +116,7 @@ func TestAccGithubOrganizationCustomProperties(t *testing.T) {
 		t.Parallel()
 
 		description := "Test Description Import"
-		propertyName := "Test"
+		propertyName := fmt.Sprintf("%s%s", testResourcePrefix, acctest.RandString(5))
 		valueType := "string"
 
 		config := fmt.Sprintf(`
@@ -125,20 +126,13 @@ func TestAccGithubOrganizationCustomProperties(t *testing.T) {
 			value_type = "%s"
 			}`, description, propertyName, valueType)
 
-		check := resource.ComposeTestCheckFunc(
-			resource.TestCheckResourceAttr(
-				"github_organization_custom_properties.test",
-				"description", description,
-			),
-		)
-
 		resource.Test(t, resource.TestCase{
 			PreCheck:          func() { skipUnlessHasOrgs(t) },
 			ProviderFactories: providerFactories,
 			Steps: []resource.TestStep{
 				{
 					Config: config,
-					Check:  check,
+					Check:  resource.TestCheckResourceAttr("github_organization_custom_properties.test", "description", description),
 				},
 				{
 					ResourceName:      "github_organization_custom_properties.test",
@@ -152,19 +146,21 @@ func TestAccGithubOrganizationCustomProperties(t *testing.T) {
 	t.Run("creates custom property with values_editable_by without error", func(t *testing.T) {
 		t.Parallel()
 
-		config := `
+		name := fmt.Sprintf("%s%s", testResourcePrefix, acctest.RandString(5))
+
+		config := fmt.Sprintf(`
 		resource "github_organization_custom_properties" "test" {
-			property_name       = "TestValuesEditableBy"
+			property_name       = "%s"
 			value_type          = "string"
 			required            = false
 			description         = "Test property for values_editable_by"
 			values_editable_by  = "org_and_repo_actors"
-		}`
+		}`, name)
 
 		check := resource.ComposeTestCheckFunc(
 			resource.TestCheckResourceAttr(
 				"github_organization_custom_properties.test",
-				"property_name", "TestValuesEditableBy",
+				"property_name", name,
 			),
 			resource.TestCheckResourceAttr(
 				"github_organization_custom_properties.test",
@@ -187,18 +183,20 @@ func TestAccGithubOrganizationCustomProperties(t *testing.T) {
 	t.Run("backward compatibility - property without values_editable_by defaults correctly", func(t *testing.T) {
 		t.Parallel()
 
-		config := `
+		name := fmt.Sprintf("%s%s", testResourcePrefix, acctest.RandString(5))
+
+		config := fmt.Sprintf(`
 		resource "github_organization_custom_properties" "test" {
-			property_name = "TestBackwardCompat"
+			property_name = "%s"
 			value_type    = "string"
 			required      = false
 			description   = "Test property without values_editable_by"
-		}`
+		}`, name)
 
 		check := resource.ComposeTestCheckFunc(
 			resource.TestCheckResourceAttr(
 				"github_organization_custom_properties.test",
-				"property_name", "TestBackwardCompat",
+				"property_name", name,
 			),
 			// When not specified, API returns "org_actors" as the default
 			resource.TestCheckResourceAttr(
@@ -222,23 +220,25 @@ func TestAccGithubOrganizationCustomProperties(t *testing.T) {
 	t.Run("update values_editable_by from org_actors to org_and_repo_actors", func(t *testing.T) {
 		t.Parallel()
 
-		configBefore := `
+		name := fmt.Sprintf("%s%s", testResourcePrefix, acctest.RandString(5))
+
+		configBefore := fmt.Sprintf(`
 		resource "github_organization_custom_properties" "test" {
-			property_name      = "TestUpdateValuesEditableBy"
+			property_name      = "%s"
 			value_type         = "string"
 			required           = false
 			description        = "Test updating values_editable_by"
 			values_editable_by = "org_actors"
-		}`
+		}`, name)
 
-		configAfter := `
+		configAfter := fmt.Sprintf(`
 		resource "github_organization_custom_properties" "test" {
-			property_name      = "TestUpdateValuesEditableBy"
+			property_name      = "%s"
 			value_type         = "string"
 			required           = false
 			description        = "Test updating values_editable_by"
 			values_editable_by = "org_and_repo_actors"
-		}`
+		}`, name)
 
 		const resourceName = "github_organization_custom_properties.test"
 
@@ -273,25 +273,25 @@ func TestAccGithubOrganizationCustomProperties(t *testing.T) {
 		// The resource config intentionally omits values_editable_by to verify
 		// Terraform can read and maintain the existing value from the API.
 
-		configWithoutField := `
+		name := fmt.Sprintf("%s%s", testResourcePrefix, acctest.RandString(5))
+
+		configWithoutField := fmt.Sprintf(`
 		resource "github_organization_custom_properties" "test" {
-			property_name = "TestImportWithUISet"
+			property_name = "%s"
 			value_type    = "string"
 			required      = false
 			description   = "Test property set via UI"
-		}`
+		}`, name)
 
 		// After import, we explicitly set the value in config to match what's in the API
-		configWithField := `
+		configWithField := fmt.Sprintf(`
 		resource "github_organization_custom_properties" "test" {
-			property_name      = "TestImportWithUISet"
+			property_name      = "%s"
 			value_type         = "string"
 			required           = false
 			description        = "Test property set via UI"
 			values_editable_by = "org_and_repo_actors"
-		}`
-
-		const resourceName = "github_organization_custom_properties.test"
+		}`, name)
 
 		resource.Test(t, resource.TestCase{
 			PreCheck:          func() { skipUnlessHasOrgs(t) },
@@ -301,7 +301,7 @@ func TestAccGithubOrganizationCustomProperties(t *testing.T) {
 					// First, create a property with values_editable_by set
 					Config: configWithField,
 					Check: resource.ComposeTestCheckFunc(
-						resource.TestCheckResourceAttr(resourceName, "values_editable_by", "org_and_repo_actors"),
+						resource.TestCheckResourceAttr("github_organization_custom_properties.test", "values_editable_by", "org_and_repo_actors"),
 					),
 				},
 				{
@@ -311,14 +311,14 @@ func TestAccGithubOrganizationCustomProperties(t *testing.T) {
 					Config: configWithoutField,
 					Check: resource.ComposeTestCheckFunc(
 						// Terraform should still see the value from the API
-						resource.TestCheckResourceAttr(resourceName, "values_editable_by", "org_and_repo_actors"),
+						resource.TestCheckResourceAttr("github_organization_custom_properties.test", "values_editable_by", "org_and_repo_actors"),
 					),
 				},
 				{
 					// Now add it back to the config - should be no changes needed
 					Config: configWithField,
 					Check: resource.ComposeTestCheckFunc(
-						resource.TestCheckResourceAttr(resourceName, "values_editable_by", "org_and_repo_actors"),
+						resource.TestCheckResourceAttr("github_organization_custom_properties.test", "values_editable_by", "org_and_repo_actors"),
 					),
 				},
 			},
