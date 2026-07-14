df732c978a935a126b7dbca74ffa471c22056c10
Timo Sand <timo.sand@f-secure.com>
2026-07-14 22:55:40 +0300

    Fix migration file
    Signed-off-by: Timo Sand <timo.sand@f-secure.com>

--- github/resource_github_repository_autolink_reference_migration_test.go
+++ github/resource_github_repository_autolink_reference_migration_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-github/v88/github"
+	"github.com/google/go-github/v89/github"
 )
 
 func buildMockResponsesForRepositoryAutolinkReferenceMigrationV1toV2(mockOwner, mockRepo string, wantRepoID int) []*mockResponse {
