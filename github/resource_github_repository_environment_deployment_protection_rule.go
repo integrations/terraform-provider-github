2b5bc5e971c918db36e3ad8909f04b5df56205e0
Aaron Stockmeister <stockmaj@yahoo.com>
2026-07-22 10:22:31 -0400

    fix: use go-github v89 in deployment protection rule resource
    main upgraded go-github v88 -> v89; the CreateCustomDeploymentProtectionRule
    request type moved packages. Match the rest of the provider.

--- github/resource_github_repository_environment_deployment_protection_rule.go
+++ github/resource_github_repository_environment_deployment_protection_rule.go
@@ -8,7 +8,7 @@ import (
 	"net/url"
 	"strconv"
 
-	"github.com/google/go-github/v88/github"
+	"github.com/google/go-github/v89/github"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
